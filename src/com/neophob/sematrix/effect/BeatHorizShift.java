18003906a22a7f999d9d129b9d2ef25e5e3d8c40
michu <michu@5edce287-8594-4907-b4da-7d032f6a0a0d>
2010-08-05 19:30:56 +0000

    fix effect bug
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@353 5edce287-8594-4907-b4da-7d032f6a0a0d

--- src/com/neophob/sematrix/effect/BeatHorizShift.java
+++ src/com/neophob/sematrix/effect/BeatHorizShift.java
@@ -1,23 +1,38 @@
 package com.neophob.sematrix.effect;
 
-import com.neophob.sematrix.generator.Generator;
 import com.neophob.sematrix.input.Sound;
 
-public class BeatHorizShift extends HorizShift {
+public class BeatHorizShift extends Effect {
 
 	int ammount=0;
 	
 	public BeatHorizShift() {
-		super(EffectName.RND_HORIZONTAL_SHIFT);
+		super(EffectName.BEAT_HORIZONTAL_SHIFT);
 	}
 
-	public int[] getBuffer(Generator generator) {
+	public int[] getBuffer(int[] buffer) {
 		if (Sound.getInstance().isPang()) {
-			ammount = (int)(Math.random()*generator.getInternalBufferXSize());
+			ammount = (int)(Math.random()*internalBufferXSize);
 		}
 		
-		return super.doHorizShift(generator, ammount);
+		return doHorizShift(buffer, ammount);
 	}
 	
+	private int[] doHorizShift(int[] buffer, int ammount) {
+		int[] ret = new int[buffer.length];
+
+		int x,idx=0,ofs;
+		for (int y=0; y<internalBufferYSize; y++) {
+			ofs=internalBufferXSize*y;
+			for (x=ammount; x<internalBufferXSize; x++) {
+				ret[idx++] = buffer[ofs+x];
+			}
+			for (x=0; x<ammount; x++) {
+				ret[idx++] = buffer[ofs+x];
+			}
+		}
+		return ret;
+	}
+
 
 }
--- src/com/neophob/sematrix/effect/BeatVerticalShift.java
+++ src/com/neophob/sematrix/effect/BeatVerticalShift.java
@@ -1,22 +1,41 @@
 package com.neophob.sematrix.effect;
 
-import com.neophob.sematrix.generator.Generator;
 import com.neophob.sematrix.input.Sound;
 
-public class BeatVerticalShift extends VerticalShift {
+public class BeatVerticalShift extends Effect {
 
 	int ammount=0;
 	
 	public BeatVerticalShift() {
-		super(EffectName.RND_VERTICAL_SHIFT);
+		super(EffectName.BEAT_VERTICAL_SHIFT);
 	}
 
-	public int[] getBuffer(Generator generator) {
+	public int[] getBuffer(int[] buffer) {
 		if (Sound.getInstance().isPang()) {
-			ammount = (int)(Math.random()*generator.getInternalBufferYSize());
+			ammount = (int)(Math.random()*internalBufferYSize);
 		}
-		return super.doVerticalShift(generator, ammount);
+		return doVerticalShift(buffer, ammount);
 	}
 	
+	/**
+	 * shift a image buffer vertical
+	 * @param ammount
+	 * @param generator
+	 * @return
+	 */
+	private int[] doVerticalShift(int[] buffer, int ammount) {
+		int[] ret = new int[buffer.length];
+
+		int idx=0;
+		int ofs = ammount*internalBufferXSize;
+		for (int i=ofs; i<buffer.length; i++) {
+			ret[idx++] = buffer[i];	
+		}
+		for (int i=0; i<ofs; i++) {
+			ret[idx++] = buffer[i];
+		}
+		
+		return ret;
+	}
 
 }
