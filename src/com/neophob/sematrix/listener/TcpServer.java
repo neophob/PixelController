b4f1c4a49fbb6e46e7d91d70f5d308c72c81c676
michu <michu@5edce287-8594-4907-b4da-7d032f6a0a0d>
2010-08-06 17:06:05 +0000

    prepare to restore state
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@360 5edce287-8594-4907-b4da-7d032f6a0a0d

--- src/com/neophob/sematrix/listener/TcpServer.java
+++ src/com/neophob/sematrix/listener/TcpServer.java
@@ -11,14 +11,7 @@ import processing.core.PApplet;
 import processing.net.Client;
 import processing.net.Server;
 
-import com.neophob.sematrix.effect.Tint;
-import com.neophob.sematrix.effect.Effect.EffectName;
-import com.neophob.sematrix.generator.Blinkenlights;
-import com.neophob.sematrix.generator.Image;
-import com.neophob.sematrix.generator.Generator.GeneratorName;
 import com.neophob.sematrix.glue.Collector;
-import com.neophob.sematrix.glue.OutputMapping;
-import com.neophob.sematrix.glue.Visual;
 import com.neophob.sematrix.input.Sound;
  
 public class TcpServer implements Runnable {
@@ -34,6 +27,8 @@ public class TcpServer implements Runnable {
 		CHANGE_OUTPUT_EFFECT,
 		CHANGE_FADER,
 		CHANGE_TINT,
+		SAVE_PRESENT,
+		LOAD_PRESENT,
 		BLINKEN,
 		IMAGE,
 		RANDOM
@@ -105,7 +100,7 @@ public class TcpServer implements Runnable {
 					if (msgCount==1) {
 						msg = StringUtils.removeEnd(msg, ";");
 						lastMsg="";
-						this.processMsg(StringUtils.split(msg, ' '));						
+						sendMsg(StringUtils.split(msg, ' '));						
 					//missing end of message... save it
 					} else if (msgCount==0) {
 						lastMsg=msg;
@@ -116,10 +111,23 @@ public class TcpServer implements Runnable {
 						for (String s: msgs) {
 							s = StringUtils.trim(s);
 							s = StringUtils.removeEnd(s, ";");
-							this.processMsg(StringUtils.split(s, ' '));
+							sendMsg(StringUtils.split(s, ' '));
 						}
 					}
 				}							
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param msg
+	 */
+	private void sendMsg(String[] msg) {
+		ValidCommands response = MessageProcessor.processMsg(msg);
+		if (response!=null) {
+			if (response == ValidCommands.STATUS) {
+				sendStatusToGui();
 			}
 		}
 	}
@@ -140,183 +148,7 @@ public class TcpServer implements Runnable {
 		sendFudiMsg("SND_SNARE "+snare);
 	}
 	
-	/**
-	 * 
-	 * @param msg
-	 */
-	private synchronized void processMsg(String[] msg) {
-		if (msg==null || msg.length<1) {
-			return;
-		}
-		
-		try {
-			ValidCommands cmd = ValidCommands.valueOf(msg[0]);
-			
-			switch (cmd) {
-			case STATUS:
-				sendStatusToGui();
-				break;
 
-			case CHANGE_GENERATOR_A:
-				try {
-					int a = Integer.parseInt(msg[1]);
-					int b = Integer.parseInt(msg[2]);
-					int c = Integer.parseInt(msg[3]);
-					int d = Integer.parseInt(msg[4]);
-					int e = Integer.parseInt(msg[5]);
-					Collector.getInstance().getVisual(0).setGenerator1(a);
-					Collector.getInstance().getVisual(1).setGenerator1(b);
-					Collector.getInstance().getVisual(2).setGenerator1(c);
-					Collector.getInstance().getVisual(3).setGenerator1(d);
-					Collector.getInstance().getVisual(4).setGenerator1(e);
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-			case CHANGE_GENERATOR_B:
-				try {
-					int a = Integer.parseInt(msg[1]);
-					int b = Integer.parseInt(msg[2]);
-					int c = Integer.parseInt(msg[3]);
-					int d = Integer.parseInt(msg[4]);
-					int e = Integer.parseInt(msg[5]);
-					Collector.getInstance().getVisual(0).setGenerator2(a);
-					Collector.getInstance().getVisual(1).setGenerator2(b);
-					Collector.getInstance().getVisual(2).setGenerator2(c);
-					Collector.getInstance().getVisual(3).setGenerator2(d);
-					Collector.getInstance().getVisual(4).setGenerator2(e);
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-			
-			case CHANGE_EFFECT_A:
-				try {
-					int a = Integer.parseInt(msg[1]);
-					int b = Integer.parseInt(msg[2]);
-					int c = Integer.parseInt(msg[3]);
-					int d = Integer.parseInt(msg[4]);
-					int e = Integer.parseInt(msg[5]);
-					Collector.getInstance().getVisual(0).setEffect1(a);
-					Collector.getInstance().getVisual(1).setEffect1(b);
-					Collector.getInstance().getVisual(2).setEffect1(c);
-					Collector.getInstance().getVisual(3).setEffect1(d);
-					Collector.getInstance().getVisual(4).setEffect1(e);
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-				
-			case CHANGE_EFFECT_B:
-				try {					
-					int a = Integer.parseInt(msg[1]);
-					int b = Integer.parseInt(msg[2]);
-					int c = Integer.parseInt(msg[3]);
-					int d = Integer.parseInt(msg[4]);
-					int e = Integer.parseInt(msg[5]);
-					Collector.getInstance().getVisual(0).setEffect2(a);
-					Collector.getInstance().getVisual(1).setEffect2(b);
-					Collector.getInstance().getVisual(2).setEffect2(c);
-					Collector.getInstance().getVisual(3).setEffect2(d);
-					Collector.getInstance().getVisual(4).setEffect2(e);
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-
-			case CHANGE_MIXER:
-				try {					
-					int a = Integer.parseInt(msg[1]);
-					int b = Integer.parseInt(msg[2]);
-					int c = Integer.parseInt(msg[3]);
-					int d = Integer.parseInt(msg[4]);
-					int e = Integer.parseInt(msg[5]);
-					Collector.getInstance().getVisual(0).setMixer(a);
-					Collector.getInstance().getVisual(1).setMixer(b);
-					Collector.getInstance().getVisual(2).setMixer(c);
-					Collector.getInstance().getVisual(3).setMixer(d);
-					Collector.getInstance().getVisual(4).setMixer(e);
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-
-			case CHANGE_OUTPUT:
-				try {					
-					int newFxA = Integer.parseInt(msg[1]);
-					int newFxB = Integer.parseInt(msg[2]);
-					int oldFxA = Collector.getInstance().getFxInputForScreen(0);
-					int oldFxB = Collector.getInstance().getFxInputForScreen(1);
-					if(oldFxA!=newFxA) {
-						log.log(Level.INFO,	"Change Output 0, old fx: {0}, new fx {1}", new Object[] {oldFxA, newFxA});
-						//Collector.getInstance().mapInputToScreen(0, newFxA);						
-						Collector.getInstance().getAllOutputMappings().get(0).getFader().startFade(newFxA, 0);
-					}
-					if(oldFxB!=newFxB) {
-						log.log(Level.INFO,	"Change Output 1, old fx: {0}, new fx {1}", new Object[] {oldFxB, newFxB});
-						//Collector.getInstance().mapInputToScreen(1, newFxB);
-						Collector.getInstance().getAllOutputMappings().get(1).getFader().startFade(newFxB, 1);
-					}
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-			
-			case CHANGE_OUTPUT_EFFECT:
-				try {					
-					int newFxA = Integer.parseInt(msg[1]);
-					int newFxB = Integer.parseInt(msg[2]);
-					Collector.getInstance().getAllOutputMappings().get(0).setEffect(Collector.getInstance().getEffect(newFxA));
-					Collector.getInstance().getAllOutputMappings().get(1).setEffect(Collector.getInstance().getEffect(newFxB));
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-				
-			case CHANGE_FADER:
-				try {					
-					int a = Integer.parseInt(msg[1]);
-					int b = Integer.parseInt(msg[2]);
-					Collector.getInstance().getAllOutputMappings().get(0).setFader(Collector.getInstance().getFader(a));					
-					Collector.getInstance().getAllOutputMappings().get(1).setFader(Collector.getInstance().getFader(b));					
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-				
-			case CHANGE_TINT:
-				try {					
-					int r = Integer.parseInt(msg[1]);
-					int g = Integer.parseInt(msg[2]);
-					int b = Integer.parseInt(msg[3]);
-					Tint t = (Tint)Collector.getInstance().getEffect(EffectName.TINT);
-					t.setColor(r, g, b);
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-			
-			case BLINKEN:
-				try {
-					String fileToLoad = msg[1];
-					Blinkenlights blink = (Blinkenlights)Collector.getInstance().getGenerator(GeneratorName.BLINKENLIGHTS);
-					blink.loadFile(fileToLoad);
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-				
-			case IMAGE:
-				try {
-					String fileToLoad = msg[1];
-					Image img = (Image)Collector.getInstance().getGenerator(GeneratorName.IMAGE);
-					img.loadFile(fileToLoad);
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-
-			case RANDOM:
-				try {
-					String onOrOff = msg[1];
-					if (onOrOff.equalsIgnoreCase("ON")) {
-						Collector.getInstance().setRandomMode(true);
-					}
-					if (onOrOff.equalsIgnoreCase("OFF")) {
-						Collector.getInstance().setRandomMode(false);
-						this.sendStatusToGui();
-					}
-				} catch (Exception e) {e.printStackTrace();}
-				break;
-				
-			default:
-				System.out.println("valid: "+cmd);
-				for (int i=1; i<msg.length; i++) System.out.println(msg[i]);
-				break;
-			}
-		} catch (IllegalArgumentException e) {
-			log.log(Level.INFO,	"Illegal argument: <{0}>", new Object[] { msg[0] });
-			return;
-		}		
-	}
 	
 	/**
 	 * 
@@ -335,37 +167,10 @@ public class TcpServer implements Runnable {
 	/**
 	 * 
 	 */
-	private void sendStatusToGui() {
-		String gen1="";
-		String gen2="";
-		String fx1="";
-		String fx2="";
-		String mix="";
-		for (Visual v: Collector.getInstance().getAllVisuals()) {
-			gen1+=v.getGenerator1Idx()+" ";
-			gen2+=v.getGenerator2Idx()+" ";
-			fx1+=v.getEffect1Idx()+" ";
-			fx2+=v.getEffect2Idx()+" ";
-			mix+=v.getMixerIdx()+" ";					
+	public void sendStatusToGui() {
+		for (String s:Collector.getInstance().getCurrentStatus()) {
+			sendFudiMsg(s);
 		}
-		
-		String fader="";
-		String output="";
-		String outputEffect="";
-		for (OutputMapping o: Collector.getInstance().getAllOutputMappings()) {
-			fader+=o.getFader().getId()+" ";
-			output+=o.getVisualId()+" ";
-			outputEffect+=o.getEffect().getId()+" ";
-		}
-		sendFudiMsg("GENERATOR_A "+gen1);
-		sendFudiMsg("GENERATOR_B "+gen2);
-		sendFudiMsg("EFFECT_A "+fx1);
-		sendFudiMsg("EFFECT_B "+fx2);
-		sendFudiMsg("MIXER "+mix);
-		sendFudiMsg("FADER "+fader);
-		sendFudiMsg("OUTPUT "+output);
-		sendFudiMsg("OUTPUT_EFFECT "+outputEffect);
-		
 	}
 	
 	
