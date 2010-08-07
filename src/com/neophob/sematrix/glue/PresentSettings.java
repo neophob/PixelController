70afbed5dd7f111650754031f92cdbcf30753b3c
Michael Vogt <michu@neophob.com>
2010-08-07 22:14:19 +0000

    update present
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@364 5edce287-8594-4907-b4da-7d032f6a0a0d

---
+++ src/com/neophob/sematrix/glue/PresentSettings.java
@@ -0,0 +1,16 @@
+package com.neophob.sematrix.glue;
+
+import java.util.List;
+
+public class PresentSettings {
+	private List<String> present;
+
+	public List<String> getPresent() {
+		return present;
+	}
+
+	public void setPresent(List<String> present) {
+		this.present = present;
+	}
+	
+}
