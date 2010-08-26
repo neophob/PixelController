ef8942a8ecd49adaac6b5055b32d4b99883581bf
michu <michu@5edce287-8594-4907-b4da-7d032f6a0a0d>
2010-08-26 12:48:41 +0000

    fix present fader
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@469 5edce287-8594-4907-b4da-7d032f6a0a0d

--- src/com/neophob/sematrix/fader/SlideLeftRight.java
+++ src/com/neophob/sematrix/fader/SlideLeftRight.java
@@ -8,32 +8,39 @@ public class SlideLeftRight extends Fader {
 	public SlideLeftRight() {
 		super(FaderName.SLIDE_LEFT_RIGHT, 1500);
 	}
-	
+
 	@Override
 	public int[] getBuffer(int[] buffer) {
-		currentStep++;		
-		int[] newBuffer = getNewBuffer();
-		if (super.isDone()) {
-			return newBuffer;
+		currentStep++;	
+
+		try {
+			int[] newBuffer = getNewBuffer();
+			if (super.isDone()) {
+				return newBuffer;
+			}
+
+			int[] ret = new int[buffer.length];		
+			float f = getCurrentStep();
+			Generator g = Collector.getInstance().getGenerator(0);
+			int ammount=(int)(g.getInternalBufferXSize()*f);
+			int ofs,x,idx=0;
+
+			int linesize=g.getInternalBufferXSize();
+			for (int y=0; y<g.getInternalBufferYSize(); y++) {
+				ofs=g.getInternalBufferXSize()*y;
+				for (x=0; x<ammount; x++) {
+					ret[idx++] = newBuffer[ofs+(linesize-ammount+x)];
+				}
+				for (x=ammount; x<g.getInternalBufferXSize(); x++) {
+					ret[idx++] = buffer[ofs+x];
+				}
+			}
+			return ret;
+		} catch (Exception e) {
+			super.setDone();
+			return buffer;
 		}
 
-		int[] ret = new int[buffer.length];		
-		float f = getCurrentStep();
-		Generator g = Collector.getInstance().getGenerator(0);
-		int ammount=(int)(g.getInternalBufferXSize()*f);
-		int ofs,x,idx=0;
-		
-		int linesize=g.getInternalBufferXSize();
-		for (int y=0; y<g.getInternalBufferYSize(); y++) {
-			ofs=g.getInternalBufferXSize()*y;
-			for (x=0; x<ammount; x++) {
-				ret[idx++] = newBuffer[ofs+(linesize-ammount+x)];
-			}
-			for (x=ammount; x<g.getInternalBufferXSize(); x++) {
-				ret[idx++] = buffer[ofs+x];
-			}
-		}
-		return ret;
 	}
 
 }
--- src/com/neophob/sematrix/fader/SlideUpsideDown.java
+++ src/com/neophob/sematrix/fader/SlideUpsideDown.java
@@ -8,31 +8,38 @@ public class SlideUpsideDown extends Fader {
 	public SlideUpsideDown() {
 		super(FaderName.SLIDE_UPSIDE_DOWN, 1500);
 	}
-	
+
 	@Override
 	public int[] getBuffer(int[] buffer) {
 		currentStep++;		
 
-		int[] newBuffer = getNewBuffer();
-		if (super.isDone()) {
-			return newBuffer;
+		try {
+			int[] newBuffer = getNewBuffer();
+			if (super.isDone()) {
+				return newBuffer;
+			}
+
+			int[] ret = new int[buffer.length];
+
+			float f = getCurrentStep();
+
+			Generator g = Collector.getInstance().getGenerator(0);
+			int ammount=(int)(g.getInternalBufferYSize()*f)*g.getInternalBufferXSize();
+			int totalSize=g.getInternalBufferYSize()*g.getInternalBufferXSize();
+			for (int y=0; y<ammount; y++) {
+				ret[y]=newBuffer[totalSize-ammount+y];
+			}
+			int idx=0;
+			for (int y=ammount; y<totalSize; y++) {
+				ret[y]=buffer[idx++];
+			}
+			return ret;
+			
+		} catch (Exception e) {
+			super.setDone();
+			return buffer;
 		}
 
-		int[] ret = new int[buffer.length];
-		
-		float f = getCurrentStep();
-
-		Generator g = Collector.getInstance().getGenerator(0);
-		int ammount=(int)(g.getInternalBufferYSize()*f)*g.getInternalBufferXSize();
-		int totalSize=g.getInternalBufferYSize()*g.getInternalBufferXSize();
-		for (int y=0; y<ammount; y++) {
-			ret[y]=newBuffer[totalSize-ammount+y];
-		}
-		int idx=0;
-		for (int y=ammount; y<totalSize; y++) {
-			ret[y]=buffer[idx++];
-		}
-		return ret;
 	}
 
 }
