62743f05cc72a3a007a96cd780b286dab6973b39
Michael Vogt <michu@neophob.com>
2010-09-02 16:04:40 +0000

    update tcp server
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@472 5edce287-8594-4907-b4da-7d032f6a0a0d

--- src/com/neophob/sematrix/listener/TcpServer.java
+++ src/com/neophob/sematrix/listener/TcpServer.java
@@ -2,6 +2,8 @@ package com.neophob.sematrix.listener;
 
 import java.net.BindException;
 import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,9 +15,9 @@ import processing.net.Server;
 
 import com.neophob.sematrix.glue.Collector;
 import com.neophob.sematrix.input.Sound;
- 
+
 public class TcpServer implements Runnable {
-	
+
 	public enum ValidCommands {
 		STATUS,
 		CHANGE_GENERATOR_A,
@@ -40,33 +42,36 @@ public class TcpServer implements Runnable {
 		RANDOM
 	}
 
+	private static final long CONNECT_RETRY_IN_MS = 8000;
+
 	private static Logger log = Logger.getLogger(TcpServer.class.getName());
-	
+
 	private Server tcpServer=null;
 	private Client client;
 	private String lastMsg="";
-	
-	private boolean clientIsConnected=true;
+
 	private Thread runner;
 	private PApplet app;
 	private int sendPort;
 	private String sendHost;
 	private int count=0;
-	
+	private long lastConnectTimestamp;
+	private boolean pdClientConnected=false;
+
 	public TcpServer(PApplet app, int listeningPort, String sendHost, int sendPort) throws BindException {
 		this.app = app;
 		app.registerDispose(this);
-		
+
 		this.sendHost = sendHost;
 		this.sendPort = sendPort;
-		
+
 		try {
 			serverWrapper(listeningPort);
 			log.log(Level.INFO,	"Server started at port {0}", new Object[] { listeningPort });
 			this.runner = new Thread(this);
 			this.runner.setName("ZZ TCP Server");
 			this.runner.start();
-					
+
 			connectToClient();
 			if (client==null) {
 				log.log(Level.INFO,	"Pure Data Client not available yet!");
@@ -75,11 +80,11 @@ public class TcpServer implements Runnable {
 			log.log(Level.INFO,	"Failed to start TCP Server {0}", new Object[] { e });
 		}				
 	}
-	
+
 	public void dispose() {
 		runner = null;
 	}
-	
+
 	/**
 	 * 
 	 */
@@ -93,7 +98,7 @@ public class TcpServer implements Runnable {
 					if (Collector.getInstance().isRandomMode() && (count%20)==2) {
 						sendStatusToGui();					
 					}
-					
+
 					Client c = tcpServer.available();
 					if (c!=null && c.available()>0) {					
 						String msg = lastMsg+StringUtils.replace(c.readString(), "\n", "");
@@ -105,10 +110,10 @@ public class TcpServer implements Runnable {
 							msg = StringUtils.removeEnd(msg, ";");
 							lastMsg="";
 							sendMsg(StringUtils.split(msg, ' '));						
-						//missing end of message... save it
+							//missing end of message... save it
 						} else if (msgCount==0) {
 							lastMsg=msg;
-						//more than one message receieved, split it
+							//more than one message receieved, split it
 						} else {
 							lastMsg="";
 							String[] msgs = msg.split(";");
@@ -130,7 +135,7 @@ public class TcpServer implements Runnable {
 
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param msg
@@ -143,7 +148,7 @@ public class TcpServer implements Runnable {
 			}
 		}
 	}
-	
+
 	/**
 	 * send beat detection to gui
 	 */
@@ -159,32 +164,44 @@ public class TcpServer implements Runnable {
 		sendFudiMsg("SND_KICK "+kick);
 		sendFudiMsg("SND_SNARE "+snare);
 	}
-	
 
-	
+
+
 	/**
 	 * 
 	 * @param msg
 	 */
 	private synchronized void sendFudiMsg(String msg) {
 		try {
-			if (client==null) {
+
+			long l = System.currentTimeMillis()-lastConnectTimestamp;
+			if (client==null &&  l > CONNECT_RETRY_IN_MS) {
 				connectToClient();
 			}
 
-			if (client!=null /*&& client.active()*/) {
-				clientIsConnected=true;
-				client.write(msg+";");	
-			}
-			else if (clientIsConnected) {
-				log.warning("No Client available!");
-				clientIsConnected=false;
+			if (client!=null && client.active()) {
+				writeToClient(msg+";");	
 			}
 		} catch (Exception e) {
-			log.warning("Failed to send data to the client");
+			//client disconnected!
+			if (pdClientConnected) {
+				log.warning("Failed to send data to the pure data client: "+e.getMessage());
+				pdClientConnected=false;
+				client.dispose();
+				client=null;
+			}
 		}
 	}
-	
+
+	/**
+	 * send data to client and handle exceptions!
+	 * @param s
+	 * @throws Exception
+	 */
+	private void writeToClient(String s) throws Exception {
+		client.output.write(s.getBytes());
+	}
+
 	/**
 	 * 
 	 */
@@ -193,31 +210,31 @@ public class TcpServer implements Runnable {
 			sendFudiMsg(s);
 		}
 	}
-	
-	
+
+
 	/**
 	 * 
 	 */
 	private void connectToClient() {
+		Socket socket = new Socket();
+		lastConnectTimestamp = System.currentTimeMillis();
 		try {
-			//client = new Client(app, sendHost, sendPort);
-			clientConnectionWrapper();
-			log.log(Level.INFO,	"Pure Data Client connected!");
+			socket.connect(new InetSocketAddress(sendHost, sendPort), 4000);
+			client = new Client(app, socket);
+			log.log(Level.INFO,	"Pure Data Client connected at "+sendHost+":"+sendPort+"!");
+			pdClientConnected=true;
 		} catch (Exception e) {
-			log.log(Level.WARNING, "Pure Data Client not found!");
-			client = null;			
+			log.log(Level.WARNING, "Pure Data Client not found at "+sendHost+":"+sendPort);
+			pdClientConnected=false;
+			client = null;
+			if (socket!=null) {
+				try {
+					socket.close();
+				} catch (Exception e2) {}
+			}
 		}		
 	}
-	
-	/**
-	 * stupid wrapper, as processing does not declare throws!
-	 * @throws ConnectException
-	 * @throws BindException
-	 */
-	private void clientConnectionWrapper() throws ConnectException, BindException {
-		client = new Client(app, sendHost, sendPort);
-	}
-	
+
 	/**
 	 * 
 	 * @param listeningPort
