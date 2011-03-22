5ca34f5a84119343704f3c54112f6444292a2edd
Michael Vogt <michu@neophob.com>
2011-03-22 21:01:54 +0000

    add endless zoom
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@573 5edce287-8594-4907-b4da-7d032f6a0a0d

--- src/com/neophob/sematrix/effect/RotoZoom.java
+++ src/com/neophob/sematrix/effect/RotoZoom.java
@@ -1,31 +1,109 @@
 package com.neophob.sematrix.effect;
 
+import com.neophob.sematrix.fader.CrossfaderHelper;
+
 
 /**
  * 
  * @author michu
+ * 
+ * TODO: endless zoom, nachdem der zoom weit im bild ist, ein crossfade auf das ursprŸngliche bild
+ * 
  * ripped from http://www.openprocessing.org/visuals/?visualID=8030
  */
 public class RotoZoom extends Effect {
-
-	private float angle;
-	private float scale;
-	private float dscalee=0.01f;
 	
+	public enum WORKMODE {
+		PINGPONG,
+		ZOOM
+	}
+	
+	private float angle;
+	private float scale, scale2;
+	private float scaleOrig;
+	private float faderPos;
+	private float dscalee=0.03f;
+	
+	private WORKMODE workmode = WORKMODE.ZOOM;
+
+	private int[] rotoZoomedBuffer=null;
+	
+	/**
+	 * 
+	 * @param scale
+	 * @param angle
+	 */
 	public RotoZoom(float scale, float angle) {
 		super(EffectName.ROTOZOOM);
 		this.scale = scale;
+		this.scaleOrig = scale;
 		this.angle = angle;
+		this.faderPos = 0.0f;		
 	}
 
-	public int[] getBuffer(int[] buffer) {
-		int[] ret = new int[buffer.length];
 
+	
+	/**
+	 * 
+	 */
+	public int[] getBuffer(int[] buffer) {		
+		//lazy init buffer
+		if (this.rotoZoomedBuffer==null) {
+			this.rotoZoomedBuffer = new int[buffer.length];				
+		}
+		
+		this.rotoZoomedBuffer = rotoZoom(scale, angle, buffer);
+		
+		
+		if (workmode == WORKMODE.ZOOM && faderPos>0.0f) {			
+			return CrossfaderHelper.getBuffer(faderPos, this.rotoZoomedBuffer, rotoZoom(scale2, angle, buffer));
+		}
+		return this.rotoZoomedBuffer;
+	}
+	
+
+	/**
+	 * 
+	 */
+	public void update() {
+		angle+=0.02f;
+		scale-=dscalee;
+
+		if (workmode == WORKMODE.ZOOM) {
+			if (this.scale < 0.4f) {
+				faderPos += 0.04f;
+				scale2-=dscalee;
+							
+				if (faderPos>0.98f) {
+					//finished fading - reset values
+					this.faderPos = 0.0f;
+					this.scale = this.scale2;
+					this.scale2 = this.scaleOrig;
+				}			
+			}			
+		} else {
+			if (scale<0.13f || scale>1.6f) {
+				dscalee*=-1;
+			}			
+		}
+
+	}
+
+	
+	/**
+	 * do the actual roto zooming
+	 * 
+	 * @param scaleP
+	 * @param angleP
+	 * @param buffer
+	 */
+	private int[] rotoZoom(float scaleP, float angleP, int bufferP[]) {
+		int[] tmp = new int[bufferP.length];
 		int x,y,offs=0,soffs;
 		float tx,ty;
 		
-		float ca=(float)(scale*Math.cos(angle));//cosAng);
-		float sa=(float)(scale*Math.sin(angle));//sinAng);
+		float ca=(float)(scaleP*Math.cos(angleP));//cosAng);
+		float sa=(float)(scaleP*Math.sin(angleP));//sinAng);
 
 		float txx=0-(internalBufferXSize/2)*sa;
 		float tyy=0+(internalBufferYSize/2)*ca;
@@ -39,21 +117,11 @@ public class RotoZoom extends Effect {
 				tx+=ca;
 				ty+=sa;
 				soffs=(int)tx+(int)(ty)*internalBufferXSize;
-				ret[offs++]=buffer[soffs&(buffer.length-1)];
+				tmp[offs++]=bufferP[soffs&(bufferP.length-1)];
 			}
 		}
-
-		return ret;
+		
+		return tmp;
 	}
-
-	public void update() {
-		angle+=0.02f;
-		scale+=dscalee;
-//		if (scale<0.53 || scale>3) {
-		if (scale<0.13f || scale>1.6f) {
-			dscalee*=-1;
-		}		
-	}
-
 
 }
