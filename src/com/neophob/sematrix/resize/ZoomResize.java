b7f92a8ec1388d2e1d612ea2ce99ad16c7e54076
michu <michu@5edce287-8594-4907-b4da-7d032f6a0a0d>
2011-04-03 20:30:49 +0000

    refactoring resize
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@586 5edce287-8594-4907-b4da-7d032f6a0a0d

---
+++ src/com/neophob/sematrix/resize/ZoomResize.java
@@ -0,0 +1,20 @@
+package com.neophob.sematrix.resize;
+
+
+/**
+ * 
+ * 
+ * @author michu
+ *
+ */
+public class ZoomResize extends Resize {
+
+	public ZoomResize() {
+		super(ResizeName.ZOOM_RESIZE);
+	}
+	
+	public int[] getBuffer(int[] buffer, int deviceXSize, int deviceYSize, int currentXSize, int currentYSize) {
+		//TODO
+		return null;
+	}
+}
