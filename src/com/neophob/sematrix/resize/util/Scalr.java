388d1ff2652154c648335bb8aeb056c7b403b31f
Michael Vogt <michu@neophob.com>
2011-06-10 23:49:40 +0000

    add header to each file
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@656 5edce287-8594-4907-b4da-7d032f6a0a0d

--- src/com/neophob/sematrix/resize/util/Scalr.java
+++ src/com/neophob/sematrix/resize/util/Scalr.java
@@ -1,18 +1,22 @@
-/**   
- * Copyright 2010 The Buzz Media, LLC
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/**
+ * Copyright (C) 2011 Michael Vogt <michu@neophob.com>
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * This file is part of PixelController.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * PixelController is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PixelController is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PixelController.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.neophob.sematrix.resize.util;
 
 import java.awt.Graphics2D;
