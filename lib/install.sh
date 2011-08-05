a0bd5041c75c8f107a6c3ceeb3026e4c81b0b481
michu <michu@5edce287-8594-4907-b4da-7d032f6a0a0d>
2011-08-05 23:58:17 +0000

    fix some maven issues
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@706 5edce287-8594-4907-b4da-7d032f6a0a0d

--- lib/install.sh
+++ lib/install.sh
@@ -1,3 +1,7 @@
+#########################
+# most packages are not available on maven repos. this means we need to install them on
+# our local repo 
+
 mvn install:install-file -Dfile=./blinkenlights.jar -DgroupId=neophob.com -DartifactId=processing-blinkenlight -Dversion=0.5 -Dpackaging=jar
 mvn install:install-file -Dfile=./neorainbowduino.jar -DgroupId=neophob.com -DartifactId=neorainbowduino -Dversion=0.82 -Dpackaging=jar
 
@@ -7,7 +11,16 @@ mvn install:install-file -Dfile=./net.jar -DgroupId=processing.org -DartifactId=
 mvn install:install-file -Dfile=./pde.jar -DgroupId=processing.org -DartifactId=pde -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=./jna.jar -DgroupId=processing.org -DartifactId=jna -Dversion=1.0 -Dpackaging=jar
 
+cd serial
 mvn install:install-file -Dfile=./serial.jar -DgroupId=processing.org -DartifactId=serial -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=./RXTXcomm.jar -DgroupId=qbang.org -DartifactId=rxtx -Dversion=2.2pre5 -Dpackaging=jar
 
+cd ..
+cd minim
 mvn install:install-file -Dfile=./minim.jar -DgroupId=compartmental.net -DartifactId=minim -Dversion=2.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./jsminim.jar -DgroupId=compartmental.net -DartifactId=jsminim -Dversion=2.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./minim-spi.jar -DgroupId=compartmental.net -DartifactId=minim-spi -Dversion=2.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./tritonus_aos.jar -DgroupId=compartmental.net -DartifactId=tritonus_aos -Dversion=2.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./tritonus_share.jar -DgroupId=compartmental.net -DartifactId=tritonus_share -Dversion=2.0.2 -Dpackaging=jar
+
+cd ..
---
+++ src/build/PixelController.sh
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2011 Michael Vogt <michu@neophob.com>
+#
+# This file is part of PixelController.
+#
+# PixelController is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PixelController is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PixelController.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#/bin/bash
+
+export JAVA_OPT="-Dcom.apple.hwaccel=false -Djava.library.path=lib/serial -server"
+source ./classpath-unix.properties
+echo classpath: $classpath
+java $JAVA_OPT -classpath $classpath:./lib/PixelController-0.9.jar com.neophob.PixelController
