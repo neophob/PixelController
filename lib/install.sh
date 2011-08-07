f2446151ef77f13996c1c9aa38bdea1450705fbd
Michael Vogt <michu@neophob.com>
2011-08-07 11:04:41 +0000

    cleanup pom, add repo, fix version nr
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@724 5edce287-8594-4907-b4da-7d032f6a0a0d

--- lib/install.sh
+++ lib/install.sh
@@ -2,18 +2,18 @@
 # most packages are not available on maven repos. this means we need to install them on
 # our local repo 
 
-mvn install:install-file -Dfile=./blinkenlights.jar -DgroupId=neophob.com -DartifactId=processing-blinkenlight -Dversion=0.5 -Dpackaging=jar
-mvn install:install-file -Dfile=./neorainbowduino.jar -DgroupId=neophob.com -DartifactId=neorainbowduino -Dversion=0.82 -Dpackaging=jar
+mvn install:install-file -Dfile=./blinkenlights.jar -DgroupId=com.neophob -DartifactId=processing-blinkenlight -Dversion=0.5 -Dpackaging=jar
+mvn install:install-file -Dfile=./neorainbowduino.jar -DgroupId=com.neophob -DartifactId=neorainbowduino -Dversion=0.82 -Dpackaging=jar
 
-mvn install:install-file -Dfile=./core.jar -DgroupId=processing.org -DartifactId=core -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=./ecj.jar -DgroupId=processing.org -DartifactId=ecj -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=./net.jar -DgroupId=processing.org -DartifactId=net -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=./pde.jar -DgroupId=processing.org -DartifactId=pde -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=./jna.jar -DgroupId=processing.org -DartifactId=jna -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=./core.jar -DgroupId=org.processing -DartifactId=core -Dversion=1.2.1 -Dpackaging=jar
+mvn install:install-file -Dfile=./net.jar -DgroupId=org.processing -DartifactId=net -Dversion=1.2.1 -Dpackaging=jar
+mvn install:install-file -Dfile=./pde.jar -DgroupId=org.processing -DartifactId=pde -Dversion=1.2.1 -Dpackaging=jar
+#mvn install:install-file -Dfile=./jna.jar -DgroupId=org.processing -DartifactId=jna -Dversion=1.2.1 -Dpackaging=jar
+#mvn install:install-file -Dfile=./ecj.jar -DgroupId=org.processing -DartifactId=ecj -Dversion=1.2.1 -Dpackaging=jar
 
 cd serial
-mvn install:install-file -Dfile=./serial.jar -DgroupId=processing.org -DartifactId=serial -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=./RXTXcomm.jar -DgroupId=qbang.org -DartifactId=rxtx -Dversion=2.2pre5 -Dpackaging=jar
+mvn install:install-file -Dfile=./serial.jar -DgroupId=org.processing -DartifactId=serial -Dversion=1.2.1 -Dpackaging=jar
+mvn install:install-file -Dfile=./RXTXcomm.jar -DgroupId=org.qbang -DartifactId=rxtx -Dversion=2.2pre5 -Dpackaging=jar
 
 cd ..
 cd minim
