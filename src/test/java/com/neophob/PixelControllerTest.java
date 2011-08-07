d109f7e7b0749b3e3af2feb8d3654846b8310de2
michu <michu@5edce287-8594-4907-b4da-7d032f6a0a0d>
2011-08-07 20:03:04 +0000

    correct eclipse build
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@728 5edce287-8594-4907-b4da-7d032f6a0a0d

---
+++ src/test/java/com/neophob/PixelControllerTest.java
@@ -0,0 +1,13 @@
+package com.neophob;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PixelControllerTest {
+
+	@Test
+	public void testMain() {
+		assertEquals(2,1+1);
+	}
+}
