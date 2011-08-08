68bcb452bf19fc9cba0d1dc2131468397c31f295
michu <michu@5edce287-8594-4907-b4da-7d032f6a0a0d>
2011-08-08 10:13:34 +0000

    add junit tests
    git-svn-id: https://svn.origo.ethz.ch/michu/ZZ_SeMAtrix@732 5edce287-8594-4907-b4da-7d032f6a0a0d

--- src/test/java/com/neophob/PixelControllerTest.java
+++ src/test/java/com/neophob/PixelControllerTest.java
@@ -1,13 +1,37 @@
 package com.neophob;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import com.neophob.sematrix.effect.PixelControllerEffect;
+import com.neophob.sematrix.generator.PixelControllerGenerator;
+import com.neophob.sematrix.glue.Visual;
+import com.neophob.sematrix.mixer.PixelControllerMixer;
+import com.neophob.sematrix.output.PixelControllerOutput;
+import com.neophob.sematrix.properties.PropertiesHelper;
+import com.neophob.sematrix.resize.PixelControllerResize;
 
 public class PixelControllerTest {
 
 	@Test
 	public void testMain() {
-		assertEquals(2,1+1);
+		PixelControllerResize pixelControllerResize = new PixelControllerResize();
+		pixelControllerResize.initAll();
+
+		//create generators
+		PixelControllerGenerator pixelControllerGenerator = new PixelControllerGenerator();
+		pixelControllerGenerator.initAll();
+		
+		PixelControllerEffect pixelControllerEffect = new PixelControllerEffect();
+		pixelControllerEffect.initAll();
+
+		PixelControllerMixer pixelControllerMixer = new PixelControllerMixer();
+		pixelControllerMixer.initAll();
+		
+		//create 5 visuals
+		Visual.initializeVisuals(5);
+				
+		PixelControllerOutput pixelControllerOutput = new PixelControllerOutput();
+		pixelControllerOutput.initAll();
+		
 	}
 }
---
+++ src/test/java/com/neophob/sematrix/output/RotateBufferTest.java
@@ -0,0 +1,58 @@
+package com.neophob.sematrix.output;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.neophob.sematrix.properties.DeviceConfig;
+
+/**
+ * verify the rotate buffer code
+ * @author michu
+ *
+ */
+public class RotateBufferTest {
+
+	private static void dumpBuffer(int[] ret) {
+		int a=0;
+		for (int r: ret) {
+			System.out.print(r+", ");
+			a++;
+			if (a==8) {System.out.println();a=0;}
+		}
+	}
+	
+	@Test
+	public void rotateTest() {
+		int[] ret;
+		int[] buffer = new int[] {
+			1,2,3,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,4
+		};
+		
+		ret = RotateBuffer.transformImage(buffer, DeviceConfig.NO_ROTATE);
+		assertEquals(1, ret[0]);
+
+		ret = RotateBuffer.transformImage(buffer, DeviceConfig.ROTATE_90);
+		assertEquals(1, ret[7]);
+		ret = RotateBuffer.transformImage(buffer, DeviceConfig.ROTATE_90_FLIPPEDY);
+		assertEquals(1, ret[63]);
+
+		ret = RotateBuffer.transformImage(buffer, DeviceConfig.ROTATE_180);
+		assertEquals(1, ret[63]);
+		ret = RotateBuffer.transformImage(buffer, DeviceConfig.ROTATE_180_FLIPPEDY);
+		assertEquals(1, ret[7]);
+
+		ret = RotateBuffer.transformImage(buffer, DeviceConfig.ROTATE_270);
+		assertEquals(1, ret[56]);
+		ret = RotateBuffer.transformImage(buffer, DeviceConfig.ROTATE_270_FLIPPEDY);
+		assertEquals(ret[0], 1);
+
+	}
+}
