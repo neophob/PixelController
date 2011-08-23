ab207242a6474745861021d8001be44461882167
Michael Vogt <michu@neophob.com>
2011-08-23 20:41:45 +0200

    fix by markus lang

--- lib/install.sh
+++ lib/install.sh
@@ -11,16 +11,11 @@ mvn install:install-file -Dfile=./pde.jar -DgroupId=org.processing -DartifactId=
 #mvn install:install-file -Dfile=./jna.jar -DgroupId=org.processing -DartifactId=jna -Dversion=1.2.1 -Dpackaging=jar
 #mvn install:install-file -Dfile=./ecj.jar -DgroupId=org.processing -DartifactId=ecj -Dversion=1.2.1 -Dpackaging=jar
 
-cd serial
-mvn install:install-file -Dfile=./serial.jar -DgroupId=org.processing -DartifactId=serial -Dversion=1.2.1 -Dpackaging=jar
-mvn install:install-file -Dfile=./RXTXcomm.jar -DgroupId=org.qbang -DartifactId=rxtx -Dversion=2.2pre5 -Dpackaging=jar
+mvn install:install-file -Dfile=./serial/serial.jar -DgroupId=org.processing -DartifactId=serial -Dversion=1.2.1 -Dpackaging=jar
+mvn install:install-file -Dfile=./serial/RXTXcomm.jar -DgroupId=org.qbang -DartifactId=rxtx -Dversion=2.2pre5 -Dpackaging=jar
 
-cd ..
-cd minim
-mvn install:install-file -Dfile=./minim.jar -DgroupId=compartmental.net -DartifactId=minim -Dversion=2.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=./jsminim.jar -DgroupId=compartmental.net -DartifactId=jsminim -Dversion=2.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=./minim-spi.jar -DgroupId=compartmental.net -DartifactId=minim-spi -Dversion=2.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=./tritonus_aos.jar -DgroupId=compartmental.net -DartifactId=tritonus_aos -Dversion=2.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=./tritonus_share.jar -DgroupId=compartmental.net -DartifactId=tritonus_share -Dversion=2.0.2 -Dpackaging=jar
-
-cd ..
+mvn install:install-file -Dfile=./minim/library/minim.jar -DgroupId=compartmental.net -DartifactId=minim -Dversion=2.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./minim/library/jsminim.jar -DgroupId=compartmental.net -DartifactId=jsminim -Dversion=2.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./minim/library/minim-spi.jar -DgroupId=compartmental.net -DartifactId=minim-spi -Dversion=2.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./minim/library/tritonus_aos.jar -DgroupId=compartmental.net -DartifactId=tritonus_aos -Dversion=2.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./minim/library/tritonus_share.jar -DgroupId=compartmental.net -DartifactId=tritonus_share -Dversion=2.0.2 -Dpackaging=jar
