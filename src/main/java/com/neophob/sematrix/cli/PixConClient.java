b83cb0394b62f5fcda69b7e1fb3dda94a8c208aa
Michael Vogt <michu@neophob.com>
2011-09-08 14:48:14 +0200

    add client junit tests, basic function should be ok

---
+++ src/main/java/com/neophob/sematrix/cli/PixConClient.java
@@ -0,0 +1,124 @@
+package com.neophob.sematrix.cli;
+
+import jargs.gnu.CmdLineParser;
+import jargs.gnu.CmdLineParser.IllegalOptionValueException;
+import jargs.gnu.CmdLineParser.UnknownOptionException;
+
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.text.ParseException;
+
+import processing.core.PApplet;
+import processing.net.Client;
+
+import com.neophob.sematrix.listener.MessageProcessor.ValidCommands;
+import com.neophob.sematrix.listener.TcpServer;
+
+/**
+ * The Class Client.
+ */
+public class PixConClient {
+
+    /** The Constant DEFAULT_PORT. */
+    private static final int DEFAULT_PORT = 3448;
+
+    /** The Constant DEFAULT_HOST. */
+    private static final String DEFAULT_HOST = "127.0.0.1";
+
+    private static final String PARAM_COMMAND = "command";
+    private static final String PARAM_PORT = "port";
+    private static final String PARAM_HOST = "hostname";
+
+    /**
+     * Usage.
+     *
+     * @param options the options
+     */
+    private static void usage() {
+        System.out.println("Usage: Client [-h hostname] [-p port] -c ValidCommand");
+        System.exit(1);		
+    }
+
+
+    /**
+     * Parses the argument.
+     *
+     * @param args the args
+     * @return the parsed argument
+     */
+    protected static ParsedArgument parseArgument(String[] args) {
+
+        CmdLineParser parser = new CmdLineParser();
+        CmdLineParser.Option host = parser.addStringOption('h', PARAM_HOST);
+        CmdLineParser.Option port = parser.addIntegerOption('p', PARAM_PORT);
+        CmdLineParser.Option command = parser.addStringOption('c', PARAM_COMMAND);
+
+        try {
+            parser.parse(args);
+            String pHost = (String)parser.getOptionValue(host, DEFAULT_HOST);
+            int pPort = (Integer)parser.getOptionValue(port, DEFAULT_PORT);
+            String pCmd = (String)parser.getOptionValue(command);            
+            if (pCmd==null) {
+                throw new IllegalArgumentException("no ValidCommand specified!");
+            }
+            
+            ValidCommands parsedCommand = ValidCommands.valueOf(pCmd);
+
+            String[] otherArgs = parser.getRemainingArgs();
+            String param = "";
+            for (String s: otherArgs) {
+                //System.out.println("PARAM2: "+s);
+                param += s+" ";
+            }
+
+            return new ParsedArgument(pHost, pPort, parsedCommand, param.trim());
+        } catch (UnknownOptionException e) {
+            System.err.println(e.getMessage());
+        } catch (IllegalOptionValueException e) {
+            System.err.println(e.getMessage());
+        }
+
+        usage();
+        System.exit(2);
+        return null;
+    }
+
+    private static Client connectToServer(ParsedArgument cmd) throws ConnectException {
+        Client client = null;
+        Socket socket = new Socket();
+        try {
+            socket.connect(new InetSocketAddress(cmd.getHostname(), cmd.getPort()), 2000);
+            client = new Client(new PApplet(), socket);
+            System.out.println("Connected to "+cmd.getTarget());
+            return client;
+        } catch (Exception e) {
+            System.err.println("Failed to connect Server at "+cmd.getTarget());
+            if (socket!=null) {
+                try {
+                    socket.close();
+                } catch (Exception e2) {}
+            }
+        }       
+        throw new ConnectException("Failed to connect "+cmd.getTarget());
+    }
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     * @throws ParseException the parse exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("PixelController Client v0.1");
+
+        ParsedArgument cmd = parseArgument(args);
+        System.out.println("\t"+cmd);
+        
+        Client c = connectToServer(cmd);       
+        c.write(cmd.getPayload(TcpServer.FUDI_MSG_END_MARKER));
+        
+        System.out.println("Close connection, Bye!");
+        c.dispose();
+    }
+}
---
+++ src/test/java/com/neophob/sematrix/cli/PixConClientTest.java
@@ -0,0 +1,73 @@
+package com.neophob.sematrix.cli;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.neophob.sematrix.listener.MessageProcessor.ValidCommands;
+
+/**
+ * verify the rotate buffer code
+ * @author michu
+ *
+ */
+public class PixConClientTest extends PixConClient {
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void parserTestNoParameter() {
+        ParsedArgument cmd = parseArgument(new String[0]);
+        assertEquals("127.0.0.1", cmd.getHostname());
+    }
+
+    @Test
+    public void parserTestSimple() {
+        String[] param = new String[2];
+        param[0] = "-c";
+        param[1] = "RANDOMIZE";
+
+        ParsedArgument cmd = parseArgument(param);
+
+        assertEquals("127.0.0.1", cmd.getHostname());
+        assertEquals(3448, cmd.getPort());
+        assertEquals(ValidCommands.RANDOMIZE, cmd.getCommand());
+        assertEquals("", cmd.getParameter());
+    }
+
+    @Test
+    public void parserTestSimple2() {
+        String[] param = new String[5];
+        param[0] = "-c";
+        param[1] = "RANDOMIZE";
+        param[2] = "-p";
+        param[3] = "2222";
+        param[4] = "blah";
+        
+        ParsedArgument cmd = parseArgument(param);
+
+        assertEquals("127.0.0.1", cmd.getHostname());
+        assertEquals(2222, cmd.getPort());
+        assertEquals(ValidCommands.RANDOMIZE, cmd.getCommand());
+        assertEquals("blah", cmd.getParameter());
+    }
+
+    @Test
+    public void parserTestSimple3() {
+        String[] param = new String[8];
+        param[0] = "-c";
+        param[1] = "CHANGE_THRESHOLD_VALUE";
+        param[2] = "-p";
+        param[3] = "9";        
+        param[4] = "-h";
+        param[5] = "1.2.3.4";
+        param[6] = "1";
+        param[7] = "2";
+        
+        ParsedArgument cmd = parseArgument(param);
+
+        assertEquals("1.2.3.4", cmd.getHostname());
+        assertEquals(9, cmd.getPort());
+        assertEquals(ValidCommands.CHANGE_THRESHOLD_VALUE, cmd.getCommand());
+        assertEquals("1 2", cmd.getParameter());
+    }
+}
