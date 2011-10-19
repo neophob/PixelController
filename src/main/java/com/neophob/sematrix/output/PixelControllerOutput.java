765a2d6a912a5b3b56d3d4f419eca7ad477ef091
pret79 <m@rkus-lang.de>
2011-10-19 15:56:58 +0000

    initial implemenation of a producer/consumer pattern that decouples the frame generation from the output updates

--- src/main/java/com/neophob/sematrix/output/PixelControllerOutput.java
+++ src/main/java/com/neophob/sematrix/output/PixelControllerOutput.java
@@ -22,6 +22,11 @@ package com.neophob.sematrix.output;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.neophob.sematrix.glue.PixelControllerElement;
 
@@ -29,15 +34,22 @@ import com.neophob.sematrix.glue.PixelControllerElement;
  * The Class PixelControllerOutput.
  */
 public class PixelControllerOutput implements PixelControllerElement {
+	
+	private static final Logger LOG = Logger.getLogger(PixelControllerOutput.class.getName());
 
 	/** The all outputs. */
 	private List<Output> allOutputs;
+	
+	private ExecutorService executorService;
+	private CountDownLatch update_endGate;
+	private CountDownLatch prepare_endGate;
 	
 	/**
 	 * Instantiates a new pixel controller output.
 	 */
 	public PixelControllerOutput() {
 		allOutputs = new CopyOnWriteArrayList<Output>();
+		this.executorService = Executors.newCachedThreadPool();
 	}
 	
 	/* (non-Javadoc)
@@ -60,13 +72,97 @@ public class PixelControllerOutput implements PixelControllerElement {
 	 */
 	@Override
 	public void update() {
-		for (Output o: allOutputs) {
-			o.update();
+		// wait for the outputs to finish their prepare() methods from the previous call of this method
+		if (this.prepare_endGate != null) {
+			try {
+				this.prepare_endGate.await();
+			} catch (InterruptedException e) {
+				LOG.log(Level.SEVERE, "waiting for all outputs to finish their prepare() method got interrupted!", e);
+			}
 		}
+		// wait for the outputs to finish their update() methods from the previous call of this method
+		if (this.update_endGate != null) {
+			try {
+				this.update_endGate.await();
+			} catch (InterruptedException e) {
+				LOG.log(Level.SEVERE, "waiting for all outputs to finish their update() method got interrupted!", e);
+			}
+		}
+
+		// check if this is the first call of this method
+		if (this.prepare_endGate == null && this.update_endGate == null) {
+			// we have to prepare the int[] buffers manually the first time. to not mess up this method
+			// even more the prepare() methods will be called directly without any additional threading
+			// overhead that will anyway be called after the first update() calls have taken place. 
+			// For the first frame it shouldn't really matter that the outputs have to wait until the
+			// int[] buffers preparation is done.
+			for (Output output : this.allOutputs) {
+				output.prepare();
+			}
+		}
+		
+		// after the prepare() and update() methods call of all outputs are done we have in every
+		// output the currentBufferMap instance that contains all int[] buffer that just have been
+		// written to the output instances and can therefore be cleaned. also we have the preparedBufferMap
+		// instance containing the new set of int[] buffers to be written to the output. therefore we have
+		// switch both map instances to be ready for the next call of this method
+		for (Output output : this.allOutputs) {
+			output.switchBuffers();
+		}
+
+		// create countDownLatches used to call all update() and prepare() methods simultaneously
+		// and to block until all calls have been finished via the end gate instances
+		final CountDownLatch update_startGate = new CountDownLatch(1);
+		this.update_endGate = new CountDownLatch(this.allOutputs.size());
+		final CountDownLatch prepare_startGate = new CountDownLatch(1);
+		this.prepare_endGate = new CountDownLatch(this.allOutputs.size());
+
+		// construct two runnable instance for each output and schedule them
+		for (final Output output: allOutputs) {
+			// create runnable instance for updating an output instance
+			Runnable update_runnable = new Runnable() {
+				@Override
+				public void run() {
+					try {
+						update_startGate.await();
+						try {
+							output.update();
+						} finally {
+							update_endGate.countDown();
+						}
+					} catch (InterruptedException e) {
+						LOG.log(Level.SEVERE, "waiting for start gate of output: " + output.getClass().getSimpleName()  + " got interrupted!", e);
+					}
+				}
+			};
+			this.executorService.execute(update_runnable);
+			// create runnable instance for preparing an output instance
+			Runnable prepare_runnable = new Runnable() {
+				@Override
+				public void run() {
+					try {
+						prepare_startGate.await();
+						try {
+							output.prepare();
+						} finally {
+							prepare_endGate.countDown();
+						}
+					} catch (InterruptedException e) {
+						LOG.log(Level.SEVERE, "waiting for start gate of output: " + output.getClass().getSimpleName()  + " got interrupted!", e);
+					}
+				}
+			};
+			this.executorService.execute(prepare_runnable);
+		}
+		
+		// trigger output update() methods and write the 
+		// current int[] buffers to the output instances
+		update_startGate.countDown();
+		
+		// trigger output prepare() methods to be ready for
+		// the next run in parallel to the running update() methods
+		prepare_startGate.countDown();
 	}
-
-
-
 
 	/*
 	 * OUTPUT ======================================================
@@ -89,6 +185,4 @@ public class PixelControllerOutput implements PixelControllerElement {
 	public void addOutput(Output output) {
 		allOutputs.add(output);
 	}
-
-
 }
