c40e69daa66c8145d2ee86dd8a1ce878ebffa3d0
Michael Vogt <michu@neophob.com>
2011-10-28 22:13:08 +0200

    try to support multiple arnet devices

--- src/main/java/com/neophob/sematrix/output/ArtnetDevice.java
+++ src/main/java/com/neophob/sematrix/output/ArtnetDevice.java
@@ -20,6 +20,7 @@
 
 package com.neophob.sematrix.output;
 
+import java.net.InetAddress;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -41,11 +42,14 @@ public class ArtnetDevice extends OnePanelResolutionAwareOutput {
 
 	private static final Logger LOG = Logger.getLogger(ArtnetDevice.class.getName());
 
-	private int sequenceID = 0;
+	private static final int PIXELS_PER_DMX_UNIVERSE = 170;
+	
+	private int sequenceID;
+	private int nrOfUniverse;
 	private ArtNet artnet;
 	private boolean initialized;
 	
-	private String ip;
+	private InetAddress targetAdress;
 
 	/**
 	 * 
@@ -55,16 +59,46 @@ public class ArtnetDevice extends OnePanelResolutionAwareOutput {
 		super(OutputDeviceEnum.ARTNET, ph, controller, 8);
 
 		this.initialized = false;
-		this.artnet = new ArtNet();		
-		this.ip = ph.getArtNetIp();
+		this.artnet = new ArtNet();				
 		try {
+			String ip = ph.getArtNetIp();
 		    this.artnet.init();
 		    this.artnet.start();
+		    this.targetAdress = InetAddress.getByName(ip); 
+		    		
+		    this.nrOfUniverse = 1;
+		    int bufferSize=xResolution*yResolution;
+		    if (bufferSize > PIXELS_PER_DMX_UNIVERSE) {
+		    	while (bufferSize > PIXELS_PER_DMX_UNIVERSE) {
+		    		this.nrOfUniverse++;
+		    		bufferSize -= PIXELS_PER_DMX_UNIVERSE;
+		    	}
+		    }
+		    
 		    this.initialized = true;
-			LOG.log(Level.INFO, "ArtNet device initialized at "+ip);
+			LOG.log(Level.INFO, "ArtNet device initialized at {0}, using {1} universe and {2} pixels.", 
+					new Object[] {this.targetAdress.toString(), this.nrOfUniverse, xResolution*yResolution});
 		} catch (Exception e) {
-			LOG.log(Level.WARNING, "failed to initialize ArtNet port!");
+			LOG.log(Level.WARNING, "failed to initialize ArtNet port:", e);
 		}
+	}
+	
+	/**
+	 * 
+	 * @param frameBuf
+	 * @return
+	 */
+	private byte[] convertIntToByteBuffer(int[] frameBuf) {
+		byte[] buffer = new byte[frameBuf.length*3];
+		int ofs;
+		for (int i = 0; i < frameBuf.length; i++) {
+		    ofs = i*3;
+			buffer[ofs++] = (byte) ((frameBuf[i]>>16) & 0xff);
+			buffer[ofs++] = (byte) ((frameBuf[i]>>8)  & 0xff);
+			buffer[ofs  ] = (byte) ( frameBuf[i]      & 0xff);
+		}
+		
+		return buffer;
 	}
 	
     /* (non-Javadoc)
@@ -73,7 +107,25 @@ public class ArtnetDevice extends OnePanelResolutionAwareOutput {
 	@Override
 	public void update() {
 		if (this.initialized) {
-			sendBufferToArtnetReceiver(getTransformedBuffer());
+			if (this.nrOfUniverse == 1) {
+				sendBufferToArtnetReceiver(0, convertIntToByteBuffer(getTransformedBuffer()) );
+			} else {
+				int[] fullBuffer = getTransformedBuffer();				
+				int remainingInt = fullBuffer.length;
+				int ofs=0;
+				for (int i=0; i<this.nrOfUniverse; i++) {
+					int tmp=PIXELS_PER_DMX_UNIVERSE;
+					if (remainingInt<PIXELS_PER_DMX_UNIVERSE) {
+						tmp = remainingInt;
+					}
+					int[] buffer = new int[tmp];
+					System.arraycopy(fullBuffer, ofs, buffer, 0, tmp);
+					remainingInt-=tmp;
+					ofs+=tmp;
+					sendBufferToArtnetReceiver(i, convertIntToByteBuffer(buffer));
+				}
+			}
+			
 		}
 	}
 
@@ -86,21 +138,15 @@ public class ArtnetDevice extends OnePanelResolutionAwareOutput {
 	 * @param artnetReceiver
 	 * @param frameBuf
 	 */
-	private void sendBufferToArtnetReceiver(int[] frameBuf) {
+	private void sendBufferToArtnetReceiver(int universeId, byte[] buffer) {
 		ArtDmxPacket dmx = new ArtDmxPacket();
-		dmx.setUniverse(0, 0);
+		
+		//parameter: int subnetID, int universeID
+		dmx.setUniverse(0, universeId);
 		dmx.setSequenceID(sequenceID % 255);
-		byte[] buffer = new byte[frameBuf.length *3];
-		int ofs;
-		for (int i = 0; i < frameBuf.length; i++) {
-		    ofs = i*3;
-			buffer[ofs++] = (byte) ((frameBuf[i]>>16) & 0xff);
-			buffer[ofs++] = (byte) ((frameBuf[i]>>8)  & 0xff);
-			buffer[ofs  ] = (byte) ( frameBuf[i]      & 0xff);
-		}
 		dmx.setDMX(buffer, buffer.length);
-		artnet.unicastPacket(dmx, ip);
-		sequenceID++;
+		this.artnet.unicastPacket(dmx, this.targetAdress);
+		this.sequenceID++;
 	}
 
 	@Override
