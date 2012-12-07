1dda21dbb5967f6c469279e488815d38cbcc476d
Michael Vogt <michu@neophob.com>
2012-12-07 12:27:26 +0100

    update comments for tom2.net firmware

--- data/ArduinoFw/tpm2net/tpm2net.ino
+++ data/ArduinoFw/tpm2net/tpm2net.ino
@@ -39,32 +39,46 @@
 #include <EthernetUdp.h>  
 
 //the lpd6803 library needs the timer1 library
-#include <TimerOne.h>
+//#include <TimerOne.h>
 //#include <Neophob_LPD6803.h>
+
+//get the lib here: https://github.com/neophob/WS2801-Library
 #include <WS2801.h>
 
 //#if UDP_TX_PACKET_MAX_SIZE < 64
 //#error UDP packet size to small - modify UDP_TX_PACKET_MAX_SIZE in the file EthernetUdp.h and set buffers to 64 bytes 
 //#endif
 
-#define LISTENING_PORT 65506
+//define some tpm constants
+#define TPM2NET_LISTENING_PORT 65506
+#define TPM2NET_HEADER_SIZE 5
+#define TPM2NET_HEADER_IDENT 0x9c
+#define TPM2NET_CMD_DATAFRAME 0xda
+#define TPM2NET_CMD_COMMAND 0xc0
+#define TPM2NET_CMD_ANSWER 0xaa
+#define TPM2NET_FOOTER_IDENT 0x36
 
 #define NR_OF_PANELS 4
 #define PIXELS_PER_PANEL 64
 
-//4*64 pixels = 256*3colors = 768
+//as the arduino ethernet has only 2kb ram
+//we must limit the maximal udp packet size
+//a 64 pixel matrix needs 192 bytes data
 #define UDP_PACKET_SIZE 512
+
+// buffers for receiving and sending data
+uint8_t packetBuffer[UDP_PACKET_SIZE]; //buffer to hold incoming packet,
 
 //initialize pixels
 //Neophob_LPD6803 strip = Neophob_LPD6803(PIXELS_PER_PANEL*NR_OF_PANELS);
-WS2801 strip = WS2801(256);
+WS2801 strip = WS2801(PIXELS_PER_PANEL*NR_OF_PANELS);
 
+//network stuff, TODO: add dhcp/bonjour support
 byte mac[] = { 0xBE, 0x00, 0xBE, 0x00, 0xBE, 0x01 };
 IPAddress ip(192, 168, 111, 177);
 EthernetUDP Udp;
 
-// buffers for receiving and sending data
-uint8_t packetBuffer[UDP_PACKET_SIZE]; //buffer to hold incoming packet,
+
 
 void setup() {  
   Serial.begin(115200);
@@ -72,7 +86,7 @@ void setup() {
 
   // start the Ethernet and UDP:
   Ethernet.begin(mac,ip);
-  Udp.begin(LISTENING_PORT);
+  Udp.begin(TPM2NET_LISTENING_PORT);
   memset(packetBuffer, 0, UDP_PACKET_SIZE);
 
   strip.begin();
@@ -87,7 +101,7 @@ void loop() {
   int packetSize = Udp.parsePacket();
   
   //tpm2 header size is 5 bytes
-  if (packetSize>5) {
+  if (packetSize>TPM2NET_HEADER_SIZE) {
     //TODO get packet nummer, use only if currentNr>oldNr
     
     Serial.print("Received packet of size ");
@@ -98,15 +112,15 @@ void loop() {
     
     // -- Header check
     
-    //0x9C is the ident
-    if (packetBuffer[0]!=0x9c) {
+    //check header byte
+    if (packetBuffer[0]!=TPM2NET_HEADER_IDENT) {
       Serial.print("Invalid header ident ");
       Serial.println(packetBuffer[0], HEX);
       return;
     }
     
-    //0x9C is the ident
-    if (packetBuffer[1]!=0xda) {
+    //check command
+    if (packetBuffer[1]!=TPM2NET_CMD_DATAFRAME) {
       Serial.print("Invalid block type ");
       Serial.println(packetBuffer[1], HEX);
       return;
@@ -122,22 +136,20 @@ void loop() {
     Serial.println(packetNumber, HEX);
 
     uint16_t currentLed = packetNumber*PIXELS_PER_PANEL;
-    int x=5;
+    int x=TPM2NET_HEADER_SIZE;
     for (byte i=0; i < frameSize; i++) {
       strip.setPixelColor(currentLed++, packetBuffer[x], packetBuffer[x+1], packetBuffer[x+2]);
       x+=3;
     }
     strip.show();   // write all the pixels out
 
-    //0x9C is the ident
-    frameSize += 5;
-    if (packetBuffer[frameSize]!=0x36) {
+    //check footer
+    frameSize += TPM2NET_HEADER_SIZE;
+    if (packetBuffer[frameSize]!=TPM2NET_FOOTER_IDENT) {
       Serial.print("Invalid footer ident ");
       Serial.println(packetBuffer[frameSize], HEX);
       return;
     }
-
-    //do buffer stuff
   }
 }
 
@@ -145,10 +157,11 @@ void loop() {
 //     create initial image
 // --------------------------------------------
 void showInitImage() {
+  //just create some boring colors
   for (int i=0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, (i)%255, (i*2)%255, (i*4)%255);
   }    
-  // Update the strip, to start they are all 'off'
+  // Update the strip
   strip.show();
 }
 
