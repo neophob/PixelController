2a06c24dd4b9515d8fd5106d0c9c7c66511284ec
Michael Vogt <michu@neophob.com>
2013-02-26 20:03:58 +0100

    update comment

--- src/main/java/com/neophob/sematrix/properties/ValidCommands.java
+++ src/main/java/com/neophob/sematrix/properties/ValidCommands.java
@@ -124,8 +124,8 @@ public enum ValidCommands {
 	
 	CHANGE_BRIGHTNESS(CommandGroup.GENERATOR, 1, "<int> output brightness 0 .. 100"),
 	
-	OSC_GENERATOR1(CommandGroup.GENERATOR, 1, "<blob> contains 12288 bytes of image data (64x64x3)"),
-	OSC_GENERATOR2(CommandGroup.GENERATOR, 1, "<blob> contains 12288 bytes of image data (64x64x3)"),
+	OSC_GENERATOR1(CommandGroup.GENERATOR, 1, "<blob> contains 4096 bytes of image data (64x64x1bpp)"),
+	OSC_GENERATOR2(CommandGroup.GENERATOR, 1, "<blob> contains 4096 bytes of image data (64x64x1bpp)"),
 	;
 	
 	/** The nr of params. */
