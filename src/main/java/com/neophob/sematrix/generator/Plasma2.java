39566876b3a91dc5733d4d7febc14f5574557377
Michael Vogt <michu@neophob.com>
2013-10-30 00:12:20 +0100

    add logging properties file for junit test

--- src/main/java/com/neophob/sematrix/generator/Plasma2.java
+++ src/main/java/com/neophob/sematrix/generator/Plasma2.java
@@ -29,7 +29,7 @@ import com.neophob.sematrix.resize.Resize.ResizeName;
  */
 public class Plasma2 extends Generator {
 
-	private static float DEG_TO_RAD = (float)Math.PI/180.0f;
+	private static final float DEG_TO_RAD = (float)Math.PI/180.0f;
 
 	/** The frame count. */
 	private int frameCount;
