2d64a80d9159fa4c14b5a23ddf53ce3d9cb59da9
Michael Vogt <michu@neophob.com>
2013-11-05 20:07:58 +0100

    add cli package

---
+++ pixelcontroller-cli/src/main/java/com/neophob/sematrix/cli/PixelControllerCli.java
@@ -0,0 +1,13 @@
+package com.neophob.sematrix.cli;
+
+public class PixelControllerCli {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
