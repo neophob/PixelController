e3d983599ba167af50796937d1d5a7dc209302f8
Michael Vogt <michu@neophob.com>
2013-11-14 22:16:46 +0100

    cleanup

--- pixelcontroller-core/src/main/java/com/neophob/sematrix/core/osc/PixelControllerOscServer.java
+++ pixelcontroller-core/src/main/java/com/neophob/sematrix/core/osc/PixelControllerOscServer.java
@@ -23,6 +23,7 @@ import java.util.logging.Logger;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.neophob.sematrix.core.glue.Collector;
 import com.neophob.sematrix.core.jmx.PacketAndBytesStatictics;
 import com.neophob.sematrix.core.listener.MessageProcessor;
 import com.neophob.sematrix.core.properties.ValidCommands;
@@ -71,8 +72,6 @@ public class PixelControllerOscServer extends OscMessageHandler implements Packe
 		oscServer.startServer();
 	}
 	
-	//TODO stop server
-	
 	@Override
 	public void handleOscMessage(OscMessage oscIn) {
 		//sanity check
@@ -109,70 +108,13 @@ public class PixelControllerOscServer extends OscMessageHandler implements Packe
 			}			
 		}
 
-		MessageProcessor.processMsg(msg, true, oscIn.getBlob());		
+		MessageProcessor.processMsg(msg, true, oscIn.getBlob());
+		
+		//TODO does not work yet
+		//notfiy gui if an osc message arrives
+		Collector.getInstance().notifyGuiUpdate();
 	}
 
-	/**
-	 * 
-	 * @param theOscMessage
-	 */
-/*	public void oscEvent(OscMessage theOscMessage) {
-		
-		oscPacketCounter++;
-		oscBytesRecieved += theOscMessage.getBytes().length;
-		
-		//address pattern -> internal message mapping
-		String pattern = theOscMessage.addrPattern().trim().toUpperCase();
-		try {
-			//remove beginning "/"
-			if (pattern.startsWith("/")) {
-				pattern = pattern.substring(1, pattern.length());
-			}
-			ValidCommands command = ValidCommands.valueOf(pattern);
-			String[] msg = new String[1+command.getNrOfParams()];
-			msg[0] = pattern;
-			byte[] blobData = null;
-
-			for (int i=0; i<command.getNrOfParams(); i++) {
-
-				//parse osc message
-				if (theOscMessage.checkTypetag("s")) {
-					msg[i+1] = theOscMessage.get(i).stringValue();
-					LOG.log(Level.INFO,	"PARAM {0}", msg[i+1]); 
-				} else
-					if (theOscMessage.checkTypetag("i")) {
-						msg[i+1] = ""+theOscMessage.get(i).intValue();
-						LOG.log(Level.INFO,	"Received an osc message. with address pattern {0} typetag {1}.", 
-								new Object[] { theOscMessage.addrPattern(), theOscMessage.typetag() });		
-					} else
-						if (theOscMessage.checkTypetag("f")) {
-							msg[i+1] = ""+theOscMessage.get(i).floatValue();	
-							LOG.log(Level.INFO,	"Received an osc message. with address pattern {0} typetag {1}.", 
-									new Object[] { theOscMessage.addrPattern(), theOscMessage.typetag() });									
-						} else {
-							if (theOscMessage.checkTypetag("b")) {
-								//binary blob
-								blobData = theOscMessage.get(i).blobValue();	
-							} else {
-								LOG.log(Level.INFO, "Unknown typetag: "+theOscMessage.getTypetagAsBytes()[0]);
-							}
-						}
-			}
-			MessageProcessor.processMsg(msg, true, blobData);
-			
-	        //refresh gui
-			Collector.getInstance().setTriggerGuiRefresh(true);			    
-			
-		} catch (Exception e) {
-			LOG.log(Level.WARNING, "Failed to parse OSC Message", e);
-			return;
-		}		
-	}*/
-
-/*	@Override
-	public void oscStatus(OscStatus arg0) {
-		// TODO Auto-generated method stub	
-	}*/
 
     /* (non-Javadoc)
      * @see com.neophob.sematrix.core.jmx.PacketAndBytesStatictics#getPacketCounter()
