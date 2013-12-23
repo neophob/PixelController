186528ee8afe6d4d3a59d49c72fba9bba89b3d77
Michael Vogt <michu@neophob.com>
2013-12-23 20:54:23 +0100

    fix freeze

--- pixelcontroller-core/src/main/java/com/neophob/sematrix/core/api/PixelController.java
+++ pixelcontroller-core/src/main/java/com/neophob/sematrix/core/api/PixelController.java
@@ -18,92 +18,99 @@ import com.neophob.sematrix.core.visual.color.ColorSet;
  * the pixelcontroller API
  * 
  * @author michu
- *
+ * 
  */
 public interface PixelController {
 
-	final String ZEROCONF_NAME = "PixelController";
+    final String ZEROCONF_NAME = "PixelController";
 
-	/**
-	 * start pixelcontroller, initialize application and start thread
-	 */
-	void start();
-	
-	/**
-	 * shutdown pixelcontroller
-	 */
-	void stop();
-	
-	/**
-	 * 
-	 * @return true if pixelcontroller is initialized and running in the mainloop
-	 */
-	boolean isInitialized();
-	
-	/**
-	 * 
-	 * @return current framerate
-	 */
-	float getFps();
+    /**
+     * start pixelcontroller, initialize application and start thread
+     */
+    void start();
 
-	long getProcessedFrames();
+    /**
+     * shutdown pixelcontroller
+     */
+    void stop();
 
-	/**
-	 * 
-	 * @return configuration of pixelcontroller
-	 */
-	ApplicationConfigurationHelper getConfig();
-	
-	List<ColorSet> getColorSets();
-	
-	/**
-	 * 
-	 * @return pixelcontroller jmx statistics
-	 */
-	PixelControllerStatusMBean getPixConStat();
-	
-	/**
-	 * 
-	 * @return selected output
-	 */
-	IOutput getOutput();
-	List<OutputMapping> getAllOutputMappings();
-	
-	/**
-	 * 
-	 * @return pixelcontroller version
-	 */
-	String getVersion();
-	
-	/**
-	 * preset service
-	 * @return
-	 */
-	PresetService getPresetService();
-	
-	FileUtils getFileUtils();
-	
-	/**
-	 * return internal and device size
-	 * @return
-	 */
-	MatrixData getMatrix();
-	VisualState getVisualState();
-	
-	ISound getSoundImplementation();
-	
-	List<String> getGuiState();
-	
-	/**
-	 * request a gui refresh, the whole visual state will be sent
-	 */
-	void refreshGuiState();
-	
-	/**
-	 * register observer, get informed if the visual state changes
-	 * @param o
-	 */
-	void observeVisualState(Observer o);
-	void stopObserveVisualState(Observer o);
+    /**
+     * 
+     * @return true if pixelcontroller is initialized and running in the
+     *         mainloop
+     */
+    boolean isInitialized();
+
+    /**
+     * 
+     * @return current framerate
+     */
+    float getFps();
+
+    long getProcessedFrames();
+
+    /**
+     * 
+     * @return configuration of pixelcontroller
+     */
+    ApplicationConfigurationHelper getConfig();
+
+    List<ColorSet> getColorSets();
+
+    /**
+     * 
+     * @return pixelcontroller jmx statistics
+     */
+    PixelControllerStatusMBean getPixConStat();
+
+    /**
+     * 
+     * @return selected output
+     */
+    IOutput getOutput();
+
+    List<OutputMapping> getAllOutputMappings();
+
+    /**
+     * 
+     * @return pixelcontroller version
+     */
+    String getVersion();
+
+    /**
+     * preset service
+     * 
+     * @return
+     */
+    PresetService getPresetService();
+
+    FileUtils getFileUtils();
+
+    /**
+     * return internal and device size
+     * 
+     * @return
+     */
+    MatrixData getMatrix();
+
+    VisualState getVisualState();
+
+    ISound getSoundImplementation();
+
+    List<String> getGuiState();
+
+    /**
+     * request a gui refresh, the whole visual state will be sent
+     */
+    void refreshGuiState();
+
+    /**
+     * register observer, get informed if the visual state changes
+     * 
+     * @param o
+     */
+    void observeVisualState(Observer o);
+
+    void stopObserveVisualState(Observer o);
 
 }
--- pixelcontroller-core/src/main/java/com/neophob/sematrix/core/api/impl/PixelControllerServerImpl.java
+++ pixelcontroller-core/src/main/java/com/neophob/sematrix/core/api/impl/PixelControllerServerImpl.java
@@ -32,300 +32,302 @@ import com.neophob.sematrix.mdns.server.impl.MDnsServerFactory;
 /**
  * 
  * @author michu
- *
+ * 
  */
 final class PixelControllerServerImpl extends PixelControllerServer implements Runnable {
 
-	private static final Logger LOG = Logger.getLogger(PixelControllerServerImpl.class.getName());
-		
-	private VisualState visualState;
-	private PresetService presetService;
+    private static final Logger LOG = Logger.getLogger(PixelControllerServerImpl.class.getName());
 
-	private IOutput output;
-	private ISound sound;
+    private VisualState visualState;
+    private PresetService presetService;
 
-	private ApplicationConfigurationHelper applicationConfig;
-	private List<ColorSet> colorSets;
-	private FileUtils fileUtils;
-	private Framerate framerate;
+    private IOutput output;
+    private ISound sound;
 
-	private Thread runner;
-	private boolean initialized = false;
-	
-	private PixelControllerOscServer oscServer;
-	private PixelControllerStatusMBean pixConStat;
-	private PixelControllerOutput pixelControllerOutput;
+    private ApplicationConfigurationHelper applicationConfig;
+    private List<ColorSet> colorSets;
+    private FileUtils fileUtils;
+    private Framerate framerate;
 
-	private PixMDnsServer bonjour;
+    private Thread runner;
+    private boolean initialized = false;
 
+    private PixelControllerOscServer oscServer;
+    private PixelControllerStatusMBean pixConStat;
+    private PixelControllerOutput pixelControllerOutput;
 
-	/**
+    private PixMDnsServer bonjour;
+
+    /**
+     * 
+     * @param handler
+     */
+    public PixelControllerServerImpl(CallbackMessageInterface<String> handler) {
+        super(handler);
+        this.runner = new Thread(this);
+        this.runner.setName("PixelController Core");
+        this.runner.setDaemon(true);
+    }
+
+    @Override
+    public void start() {
+        LOG.log(Level.INFO, "Start PixelController v" + getVersion());
+        this.runner.start();
+    }
+
+    @Override
+    public void stop() {
+        runner = null;
+    }
+
+    @Override
+    public void run() {
+        long cnt = 0;
+
+        clientNotification("Load Configuration");
+        fileUtils = new FileUtilsLocalImpl();
+        applicationConfig = loadConfiguration(fileUtils.getDataDir());
+        this.colorSets = loadColorPalettes(fileUtils.getDataDir());
+
+        clientNotification("Create Collector");
+        LOG.log(Level.INFO, "Create Collector");
+        this.visualState = VisualState.getInstance();
+
+        clientNotification("Initialize System");
+        LOG.log(Level.INFO, "Initialize System");
+        this.pixConStat = new PixelControllerStatus((int) applicationConfig.parseFps());
+        this.sound = initSound();
+        this.presetService = new PresetServiceImpl(fileUtils.getDataDir());
+        MessageProcessor.INSTANCE.init(presetService);
+
+        this.visualState.init(fileUtils, applicationConfig, sound, colorSets, presetService);
+        framerate = new Framerate(applicationConfig.parseFps());
+
+        clientNotification("Initialize OSC Server");
+        LOG.log(Level.INFO, "Initialize OSC Server");
+
+        int listeningOscPort = applicationConfig.getOscListeningPort();
+        try {
+            if (listeningOscPort > 0) {
+                oscServer = new PixelControllerOscServer(this, listeningOscPort);
+                oscServer.startServer();
+                // register osc server in the statistic class
+                this.pixConStat.setOscServerStatistics(oscServer);
+            } else {
+                LOG.log(Level.INFO, "OSC Server disabled, port: " + listeningOscPort);
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "failed to start OSC Server", e);
+        }
+
+        try {
+            if (listeningOscPort > 0) {
+                bonjour = MDnsServerFactory.createServer(listeningOscPort, ZEROCONF_NAME);
+                bonjour.startServerAsync();
+            } else {
+                LOG.log(Level.INFO, "MDNS Server disabled, OSC port: " + listeningOscPort);
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "failed to start MDns Server", e);
+        }
+
+        clientNotification("Initialize Output device");
+        LOG.log(Level.INFO, "Initialize Output device");
+        this.output = PixelControllerOutput.getOutputDevice(this.visualState, applicationConfig);
+        if (this.output == null) {
+            throw new IllegalArgumentException("No output device found!");
+        }
+        pixelControllerOutput = new PixelControllerOutput(pixConStat);
+        pixelControllerOutput.initAll();
+        pixelControllerOutput.addOutput(this.output);
+
+        this.setupInitialConfig();
+
+        LOG.log(Level.INFO, "--- PixelController Setup END ---");
+        LOG.log(Level.INFO, "---------------------------------");
+        LOG.log(Level.INFO, "");
+
+        initialized = true;
+
+        LOG.log(Level.INFO, "Enter main loop");
+        while (Thread.currentThread() == runner) {
+            if (this.visualState.isInPauseMode()) {
+                // no update here, we're in pause mode
+                continue;
+            }
+
+            try {
+                this.visualState.updateSystem(pixConStat);
+            } catch (Exception e) {
+                LOG.log(Level.SEVERE, "VisualState.getInstance().updateSystem() failed!", e);
+            }
+
+            long l = System.currentTimeMillis();
+            pixelControllerOutput.update();
+            pixConStat.trackTime(TimeMeasureItemGlobal.OUTPUT_SCHEDULE, System.currentTimeMillis()
+                    - l);
+
+            pixConStat.setCurrentFps(framerate.getFps());
+            pixConStat.setFrameCount(cnt++);
+
+            framerate.waitForFps();
+        }
+        LOG.log(Level.INFO, "Main loop finished...");
+    }
+
+    /**
+     * @return the presetService
+     */
+    @Override
+    public PresetService getPresetService() {
+        return presetService;
+    }
+
+    /**
+     * 
+     * @param collector
+     * @param applicationConfig
+     */
+    private void setupInitialConfig() {
+        // start in random mode?
+        if (applicationConfig.startRandommode()) {
+            LOG.log(Level.INFO, "Random Mode enabled");
+            Shuffler.manualShuffleStuff();
+            visualState.setRandomMode(true);
+        }
+
+        // load saves presets
+        int presetNr = applicationConfig.loadPresetOnStart();
+        if (presetNr < 0 || presetNr >= PresetServiceImpl.NR_OF_PRESET_SLOTS) {
+            presetNr = 0;
+        }
+        LOG.log(Level.INFO, "Load preset " + presetNr);
+
+        List<String> preset = presetService.getPresets().get(presetNr).getPresent();
+        presetService.setSelectedPreset(presetNr);
+        if (preset != null) {
+            visualState.setCurrentStatus(preset);
+        } else {
+            LOG.log(Level.WARNING, "Invalid preset load on start value ignored!");
+        }
+    }
+
+    /**
+     * Initialize sound
+     * 
+     * @return
+     */
+    private ISound initSound() {
+        ISound sound;
+        // choose sound implementation
+        if (applicationConfig.isAudioAware()) {
+            try {
+                sound = new SoundMinim(applicationConfig.getSoundSilenceThreshold());
+                return sound;
+            } catch (Exception e) {
+                LOG.log(Level.WARNING, "FAILED TO INITIALIZE SOUND INSTANCE. Disable sound input.");
+            } catch (Error e) {
+                LOG.log(Level.WARNING,
+                        "FAILED TO INITIALIZE SOUND INSTANCE (Error). Disable sound input.", e);
+            }
+        }
+
+        LOG.log(Level.INFO, "Initialize dummy sound.");
+        return new SoundDummy();
+    }
+
+    /**
 	 * 
-	 * @param handler
 	 */
-	public PixelControllerServerImpl(CallbackMessageInterface<String> handler) {
-		super(handler);
-		this.runner = new Thread(this);
-		this.runner.setName("PixelController Core");
-		this.runner.setDaemon(true);
-	}
+    public String getVersion() {
+        String version = this.getClass().getPackage().getImplementationVersion();
+        if (version != null && !version.isEmpty()) {
+            return "v" + version;
+        }
+        return "Developer Snapshot";
+    }
 
-	@Override
-	public void start() {
-		LOG.log(Level.INFO, "Start PixelController v"+getVersion());
-		this.runner.start();
-	}
+    @Override
+    public boolean isInitialized() {
+        return initialized;
+    }
 
-	@Override
-	public void stop() {
-		runner = null;
-	}
+    @Override
+    public ApplicationConfigurationHelper getConfig() {
+        return applicationConfig;
+    }
 
-	@Override
-	public void run() {
-		long cnt=0;
+    @Override
+    public IOutput getOutput() {
+        return output;
+    }
 
-		clientNotification("Load Configuration");
-		fileUtils = new FileUtilsLocalImpl();
-		applicationConfig = loadConfiguration(fileUtils.getDataDir());
-		this.colorSets = loadColorPalettes(fileUtils.getDataDir());
-		
-		clientNotification("Create Collector");
-		LOG.log(Level.INFO, "Create Collector");
-		this.visualState = VisualState.getInstance();
+    @Override
+    public float getFps() {
+        return framerate.getFps();
+    }
 
-		clientNotification("Initialize System");
-		LOG.log(Level.INFO, "Initialize System");
-		this.pixConStat = new PixelControllerStatus((int)applicationConfig.parseFps());
-		this.sound = initSound();
-		this.presetService = new PresetServiceImpl(fileUtils.getDataDir());
-		MessageProcessor.INSTANCE.init(presetService);
-		
-		this.visualState.init(fileUtils, applicationConfig, sound, colorSets, presetService);     
-		framerate = new Framerate(applicationConfig.parseFps());
+    @Override
+    public PixelControllerStatusMBean getPixConStat() {
+        return pixConStat;
+    }
 
-		clientNotification("Initialize OSC Server");
-		LOG.log(Level.INFO, "Initialize OSC Server");
+    @Override
+    public MatrixData getMatrix() {
+        // TODO inject matrix to visual state!
+        return visualState.getMatrix();
+    }
 
-		int listeningOscPort = applicationConfig.getOscListeningPort();
-		try {           
-			if (listeningOscPort>0) {
-				oscServer = new PixelControllerOscServer(this, listeningOscPort);
-				oscServer.startServer();
-				//register osc server in the statistic class
-				this.pixConStat.setOscServerStatistics(oscServer);				
-			} else {
-				LOG.log(Level.INFO, "OSC Server disabled, port: "+listeningOscPort);
-			}
-		} catch (Exception e) {
-			LOG.log(Level.SEVERE, "failed to start OSC Server", e);
-		}          	   
+    /**
+     * @return the visualState
+     */
+    public VisualState getVisualState() {
+        return visualState;
+    }
 
-		try {
-			if (listeningOscPort>0) {
-				bonjour = MDnsServerFactory.createServer(listeningOscPort, ZEROCONF_NAME);
-				bonjour.startServerAsync();
-			} else {
-				LOG.log(Level.INFO, "MDNS Server disabled, OSC port: "+listeningOscPort);
-			}
-		} catch (Exception e) {
-			LOG.log(Level.SEVERE, "failed to start MDns Server", e);			
-		}
+    @Override
+    public ISound getSoundImplementation() {
+        return this.sound;
+    }
 
-		clientNotification("Initialize Output device");
-		LOG.log(Level.INFO, "Initialize Output device");
-		this.output = PixelControllerOutput.getOutputDevice(this.visualState, applicationConfig);
-		if (this.output==null) {
-			throw new IllegalArgumentException("No output device found!");
-		}
-		pixelControllerOutput = new PixelControllerOutput(pixConStat);
-		pixelControllerOutput.initAll();
-		pixelControllerOutput.addOutput(this.output);
-		
-		this.setupInitialConfig();
+    @Override
+    public long getProcessedFrames() {
+        return this.framerate.getFrameCount();
+    }
 
-		LOG.log(Level.INFO, "--- PixelController Setup END ---");
-		LOG.log(Level.INFO, "---------------------------------");
-		LOG.log(Level.INFO, "");		
+    @Override
+    public void refreshGuiState() {
+        VisualState.getInstance().notifyGuiUpdate();
+    }
 
-		initialized = true;
+    @Override
+    public void observeVisualState(Observer o) {
+        VisualState.getInstance().addObserver(o);
+    }
 
-		LOG.log(Level.INFO, "Enter main loop");
-		while (Thread.currentThread() == runner) {
-			if (this.visualState.isInPauseMode()) {
-				//no update here, we're in pause mode
-				return;
-			}
+    @Override
+    public void stopObserveVisualState(Observer o) {
+        VisualState.getInstance().deleteObserver(o);
+    }
 
-			try {
-				this.visualState.updateSystem(pixConStat);			
-			} catch (Exception e) {
-				LOG.log(Level.SEVERE, "VisualState.getInstance().updateSystem() failed!", e);
-			}
+    @Override
+    public List<ColorSet> getColorSets() {
+        return colorSets;
+    }
 
-			long l = System.currentTimeMillis();
-			pixelControllerOutput.update();
-			pixConStat.trackTime(TimeMeasureItemGlobal.OUTPUT_SCHEDULE, System.currentTimeMillis()-l);
+    @Override
+    public List<OutputMapping> getAllOutputMappings() {
+        return VisualState.getInstance().getAllOutputMappings();
+    }
 
-			pixConStat.setCurrentFps(framerate.getFps());
-			pixConStat.setFrameCount(cnt++);
+    @Override
+    public List<String> getGuiState() {
+        return VisualState.getInstance().getGuiState();
+    }
 
-			framerate.waitForFps(); 
-		}
-		LOG.log(Level.INFO, "Main loop finished...");
-	}
-
-	/**
-	 * @return the presetService
-	 */
-	@Override
-	public PresetService getPresetService() {
-		return presetService;
-	}
-
-	/**
-	 * 
-	 * @param collector
-	 * @param applicationConfig
-	 */
-	private void setupInitialConfig() {
-		//start in random mode?
-		if (applicationConfig.startRandommode()) {
-			LOG.log(Level.INFO, "Random Mode enabled");
-			Shuffler.manualShuffleStuff();
-			visualState.setRandomMode(true);
-		}
-
-		//load saves presets
-		int presetNr = applicationConfig.loadPresetOnStart();
-		if (presetNr < 0 || presetNr >= PresetServiceImpl.NR_OF_PRESET_SLOTS) {
-			presetNr=0;
-		}
-		LOG.log(Level.INFO,"Load preset "+presetNr);
-
-		List<String> preset = presetService.getPresets().get(presetNr).getPresent();
-		presetService.setSelectedPreset(presetNr);
-		if (preset!=null) { 
-			visualState.setCurrentStatus(preset);
-		} else {
-			LOG.log(Level.WARNING,"Invalid preset load on start value ignored!");
-		}		
-	}
-	
-	/**
-	 * Initialize sound
-	 * @return
-	 */
-	private ISound initSound() {
-		ISound sound;
-		//choose sound implementation
-		if (applicationConfig.isAudioAware()) {
-			try {		
-				sound = new SoundMinim(applicationConfig.getSoundSilenceThreshold());
-				return sound;
-			} catch (Exception e) {
-				LOG.log(Level.WARNING, "FAILED TO INITIALIZE SOUND INSTANCE. Disable sound input.");				
-			} catch (Error e) {
-				LOG.log(Level.WARNING, "FAILED TO INITIALIZE SOUND INSTANCE (Error). Disable sound input.", e);			
-			}			
-		} 
-
-		LOG.log(Level.INFO, "Initialize dummy sound.");
-		return new SoundDummy();
-	}
-	
-	/**
-	 * 
-	 */
-	public String getVersion() {
-		String version = this.getClass().getPackage().getImplementationVersion();
-		if (version != null && !version.isEmpty()) {
-			return "v"+version;
-		}
-		return "Developer Snapshot"; 
-	}
-
-	@Override
-	public boolean isInitialized() {
-		return initialized;
-	}
-
-	@Override
-	public ApplicationConfigurationHelper getConfig() {
-		return applicationConfig;
-	}
-
-	@Override
-	public IOutput getOutput() {
-		return output;
-	}
-
-	@Override
-	public float getFps() {
-		return framerate.getFps();		
-	}
-
-	@Override
-	public PixelControllerStatusMBean getPixConStat() {
-		return pixConStat;
-	}
-
-	@Override
-	public MatrixData getMatrix() {
-		// TODO inject matrix to visual state!		
-		return visualState.getMatrix();
-	}
-
-	/**
-	 * @return the visualState
-	 */
-	public VisualState getVisualState() {
-		return visualState;
-	}
-
-	@Override
-	public ISound getSoundImplementation() {
-		return this.sound;
-	}
-
-	@Override
-	public long getProcessedFrames() {
-		return this.framerate.getFrameCount();
-	}
-
-	@Override
-	public void refreshGuiState() {
-		VisualState.getInstance().notifyGuiUpdate();		
-	}
-
-	@Override
-	public void observeVisualState(Observer o) {
-		VisualState.getInstance().addObserver(o);		
-	}
-
-	@Override
-	public void stopObserveVisualState(Observer o) {
-		VisualState.getInstance().deleteObserver(o);		
-	}
-
-	@Override
-	public List<ColorSet> getColorSets() {
-		return colorSets;
-	}
-
-	@Override
-	public List<OutputMapping> getAllOutputMappings() {
-		return VisualState.getInstance().getAllOutputMappings();
-	}
-
-	@Override
-	public List<String> getGuiState() {
-		return VisualState.getInstance().getGuiState();
-	}
-
-	@Override
-	public FileUtils getFileUtils() {
-		return fileUtils;
-	}
+    @Override
+    public FileUtils getFileUtils() {
+        return fileUtils;
+    }
 
 }
