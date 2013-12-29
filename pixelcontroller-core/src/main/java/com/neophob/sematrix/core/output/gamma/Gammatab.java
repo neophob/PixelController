e7a79e70e6a97b783b773914bd63db2869a32e42
Michael Vogt <michu@neophob.com>
2013-12-29 22:29:09 +0100

    fix typo

--- pixelcontroller-core/src/main/java/com/neophob/sematrix/core/output/gamma/Gammatab.java
+++ pixelcontroller-core/src/main/java/com/neophob/sematrix/core/output/gamma/Gammatab.java
@@ -40,7 +40,7 @@ public final class Gammatab {
     private static int[] gamma25 = generateGammaTab(2.5f);
 
     // gamma correction found in ledstyles.de forum
-    private static int[] apecialGammaTab1 = { 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5,
+    private static int[] aspecialGammaTab1 = { 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5,
             5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16,
             17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 27, 28, 29,
             30, 30, 31, 32, 33, 34, 35, 36, 36, 37, 38, 39, 40, 41, 42, 43, 43, 44, 45, 46, 47, 48,
@@ -114,9 +114,9 @@ public final class Gammatab {
                     break;
 
                 case SPECIAL1:
-                    r = apecialGammaTab1[r];
-                    g = apecialGammaTab1[g];
-                    b = apecialGammaTab1[b];
+                    r = aspecialGammaTab1[r];
+                    g = aspecialGammaTab1[g];
+                    b = aspecialGammaTab1[b];
                     break;
 
                 case NONE:
