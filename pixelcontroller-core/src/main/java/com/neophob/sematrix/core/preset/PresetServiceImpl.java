81971cda32b94b63f7ff7db57d2434530b5a5366
Michael Vogt <michu@neophob.com>
2014-01-02 20:33:05 +0100

    rename presets.led -> presets.properties

--- pixelcontroller-core/src/main/java/com/neophob/sematrix/core/preset/PresetServiceImpl.java
+++ pixelcontroller-core/src/main/java/com/neophob/sematrix/core/preset/PresetServiceImpl.java
@@ -16,137 +16,144 @@ import org.apache.commons.lang3.StringUtils;
 /**
  * 
  * @author michu
- *
+ * 
  */
 public class PresetServiceImpl implements PresetService {
 
-	private static final Logger LOG = Logger.getLogger(PresetServiceImpl.class.getName());
+    private static final Logger LOG = Logger.getLogger(PresetServiceImpl.class.getName());
 
-    private static final String PRESETS_FILENAME = "presets.led";
+    private static final String PRESETS_FILENAME = "presets.properties";
 
-	private String filename;
-	
-	private List<PresetSettings> presets;
+    private String filename;
 
-	private int selectedPreset;
+    private List<PresetSettings> presets;
 
-	/**
-	 * 
-	 * @param fileUtils
-	 */
-	public PresetServiceImpl(String filePath) {
-		this.filename = filePath+File.separator+PRESETS_FILENAME;
-		selectedPreset=0;		
-		loadPresents();
-	}
-	
-	
-    /* (non-Javadoc)
-	 * @see com.neophob.sematrix.core.preset.PresetService#getSelectedPreset()
-	 */
-	@Override
-	public int getSelectedPreset() {
-		return selectedPreset;
-	}
+    private int selectedPreset;
 
+    /**
+     * 
+     * @param fileUtils
+     */
+    public PresetServiceImpl(String filePath) {
+        this.filename = filePath + File.separator + PRESETS_FILENAME;
+        selectedPreset = 0;
+        loadPresents();
+    }
 
-	/* (non-Javadoc)
-	 * @see com.neophob.sematrix.core.preset.PresetService#setSelectedPreset(int)
-	 */
-	@Override
-	public void setSelectedPreset(int selectedPreset) {
-		if (selectedPreset<0 || selectedPreset>NR_OF_PRESET_SLOTS) {
-			LOG.log(Level.WARNING, "Ignore invalid selected preset: "+selectedPreset);
-			return;
-		}
-		this.selectedPreset = selectedPreset;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.neophob.sematrix.core.preset.PresetService#getSelectedPreset()
+     */
+    @Override
+    public int getSelectedPreset() {
+        return selectedPreset;
+    }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.neophob.sematrix.core.preset.PresetService#setSelectedPreset(int)
+     */
+    @Override
+    public void setSelectedPreset(int selectedPreset) {
+        if (selectedPreset < 0 || selectedPreset > NR_OF_PRESET_SLOTS) {
+            LOG.log(Level.WARNING, "Ignore invalid selected preset: " + selectedPreset);
+            return;
+        }
+        this.selectedPreset = selectedPreset;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.neophob.sematrix.core.preset.PresetService#getPresets()
-	 */
-	@Override
-	public List<PresetSettings> getPresets() {
-		return presets;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.neophob.sematrix.core.preset.PresetService#getPresets()
+     */
+    @Override
+    public List<PresetSettings> getPresets() {
+        return presets;
+    }
 
-
-	/**
+    /**
      * Load presents.
      */
     private List<PresetSettings> loadPresents() {
         Properties props = new Properties();
-        
+
         presets = new ArrayList<PresetSettings>(NR_OF_PRESET_SLOTS);
-        for (int i=0; i<NR_OF_PRESET_SLOTS; i++) {
-        	presets.add(new PresetSettings());
+        for (int i = 0; i < NR_OF_PRESET_SLOTS; i++) {
+            presets.add(new PresetSettings());
         }
-        
+
         InputStream input = null;
         try {
-        	input = new FileInputStream(filename);
-            props.load(input);                        
+            input = new FileInputStream(filename);
+            props.load(input);
             String s;
-            int count=0;
-            for (int i=0; i<NR_OF_PRESET_SLOTS; i++) {
-                s=props.getProperty(""+i);
+            int count = 0;
+            for (int i = 0; i < NR_OF_PRESET_SLOTS; i++) {
+                s = props.getProperty("" + i);
                 if (StringUtils.isNotBlank(s)) {
-                	presets.get(i).setPresent(s.split(";"));
+                    presets.get(i).setPresent(s.split(";"));
                     count++;
                 }
             }
-            LOG.log(Level.INFO, "Loaded {0} presets from file {1}", new Object[] { count, PRESETS_FILENAME });
+            LOG.log(Level.INFO, "Loaded {0} presets from file {1}", new Object[] { count,
+                    PRESETS_FILENAME });
         } catch (Exception e) {
-            LOG.log(Level.WARNING, "Failed to load {0}, Error: {1}", new Object[] { PRESETS_FILENAME, e });
+            LOG.log(Level.WARNING, "Failed to load {0}, Error: {1}", new Object[] {
+                    PRESETS_FILENAME, e });
         } finally {
             try {
-                if (input!=null) {
+                if (input != null) {
                     input.close();
                 }
             } catch (Exception e) {
                 LOG.log(Level.WARNING, "Failed to close input stream", e);
-            }        
+            }
         }
-        
+
         return presets;
     }
 
-    /* (non-Javadoc)
-	 * @see com.neophob.sematrix.core.preset.PresetService#savePresents()
-	 */
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.neophob.sematrix.core.preset.PresetService#savePresents()
+     */
     @Override
-	public void savePresents() {
-    	long t1 = System.currentTimeMillis();
+    public void savePresents() {
+        long t1 = System.currentTimeMillis();
         Properties props = new Properties();
-        int idx=0;
-        for (PresetSettings p: presets) {
-            props.setProperty( ""+idx, p.getSettingsAsString() );
+        int idx = 0;
+        for (PresetSettings p : presets) {
+            props.setProperty("" + idx, p.getSettingsAsString());
             idx++;
         }
 
         OutputStream output = null;
         try {
-        	output = new FileOutputStream(filename);
+            output = new FileOutputStream(filename);
             props.store(output, "Visual Daemon presets file");
-            LOG.log(Level.INFO, "Presets saved as {0}, time needed: {1}ms", 
-            		new Object[] {PRESETS_FILENAME, (System.currentTimeMillis()-t1)});
+            LOG.log(Level.INFO, "Presets saved as {0}, time needed: {1}ms", new Object[] {
+                    PRESETS_FILENAME, (System.currentTimeMillis() - t1) });
         } catch (Exception e) {
-            LOG.log(Level.WARNING, "Failed to save {0}, Error: {1}", new Object[] { PRESETS_FILENAME, e });
+            LOG.log(Level.WARNING, "Failed to save {0}, Error: {1}", new Object[] {
+                    PRESETS_FILENAME, e });
         } finally {
             try {
-                if (output!=null) {
+                if (output != null) {
                     output.close();
                 }
             } catch (Exception e) {
                 LOG.log(Level.WARNING, "Failed to close output stream", e);
-            }        
+            }
         }
     }
 
-
-	@Override
-	public PresetSettings getSelectedPresetSettings() {		
-		return presets.get(selectedPreset);
-	}
+    @Override
+    public PresetSettings getSelectedPresetSettings() {
+        return presets.get(selectedPreset);
+    }
 }
