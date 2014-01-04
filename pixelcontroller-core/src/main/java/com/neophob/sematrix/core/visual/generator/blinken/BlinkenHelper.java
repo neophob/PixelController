1ce6976e07c5dee49f6827a3236ca0b190d497a6
Michael Vogt <michu@neophob.com>
2014-01-04 01:20:03 +0100

    speedup blinkenlight parsing

--- pixelcontroller-core/src/main/java/com/neophob/sematrix/core/visual/generator/blinken/BlinkenHelper.java
+++ pixelcontroller-core/src/main/java/com/neophob/sematrix/core/visual/generator/blinken/BlinkenHelper.java
@@ -26,115 +26,120 @@ import com.neophob.sematrix.core.visual.generator.blinken.jaxb.Blm;
 import com.neophob.sematrix.core.visual.generator.blinken.jaxb.Frame;
 import com.neophob.sematrix.core.visual.generator.blinken.jaxb.Row;
 
-
 /**
  * some blinkenlights helper functions
+ * 
  * @author michael vogt / neophob.com (c) 2010
- *
+ * 
  */
 public final class BlinkenHelper {
 
-	private static Logger log = Logger.getLogger(BlinkenHelper.class.getName());
-	
-	private BlinkenHelper() {
-		//no instance allowed
-	}
-	
-	/**
-	 * return a converted frame
-	 * @param frameNr which frame to convert
-	 * @param blm to marshalled object, our source
-	 * @return an image out of the frame
-	 */
-	public static BlinkenImage grabFrame(int frameNr, Blm blm, int color) throws NumberFormatException {
-		int frames = blm.getFrame().size();
-		
-		//some sanity checks
-		if (frameNr > frames || frameNr < 0) {
-			return null;
-		}
-		
-		int width = Integer.parseInt(blm.getWidth());
-		int height = Integer.parseInt(blm.getHeight());
-		int bits = Integer.parseInt(blm.getBits());
-		//int channels = Integer.parseInt(blm.getChannels());
-		
-		Frame f = blm.getFrame().get(frameNr);
-		List<Row> rows = f.getRow();
-		
-		BlinkenImage img = new BlinkenImage(width, height);
-		
-		/**
-		 * Structure of row data (http://blinkenlights.net/project/bml)
-		 * Each single row describes the pixel colour values in hexadecimal notation. 
-		 * If the colour depth is between 1 and 4, one hexadecimal digit is used per 
-		 * colour value (0-f). If the colour depth is between 5 and 8, two hexadecimal 
-		 * digits are used per colour value (00-ff).
-		 * 
-		 * The is one value for each pixel, one after the other. In an RGB picture with 
-		 * channels="3" there are three colour values in sequence.
-		 * 
-		 */
-		float col = (float)(color & 255)/255.f;
+    private static Logger log = Logger.getLogger(BlinkenHelper.class.getName());
 
-		for (Row r: rows) {
-			String s = r.getvalue();
-			int[] data;
-			if (bits>0 && bits<5) {
-				//one char per color value
-				data = getDataFromOneCharRow(s, col);
-			} else {
-				//two char per color value
-				data = getDataFromTwoCharRow(s);
-			}
-			if (data.length != width) {
-				log.log(Level.WARNING,
-						"Ooops: looks like here is an error: {0}!={1}"
-						, new Object[] { width, data.length });
-			}
-			
-			img.addData(data);	
-		}
+    private BlinkenHelper() {
+        // no instance allowed
+    }
 
-		return img;
-	}
-	
-	/**
-	 * convert string data to int[]
-	 * @param data
-	 * @param r colorize the pixel
-	 * @param g colorize the pixel
-	 * @param b colorize the pixel
-	 * @return 
-	 */
-	private static int[] getDataFromOneCharRow(String data, float col) {
-		int[] ret = new int[data.length()];
-		int ofs=0;
-		for (char c: data.toCharArray()) {
-			int i=Integer.parseInt(c+"", 16);
-			ret[ofs++] = (int)(i*16*col);
-		}
-		return ret;
-	}
-	
-	/**
-	 * convert string data to int[]
-	 * @param data
-	 * @return
-	 */
-	private static int[] getDataFromTwoCharRow(String data) {
-		int[] ret = new int[data.length()/2];
-		char[] convertedData = data.toCharArray();
-		int ofs=0;
-		int dst=0;
-		int col;
-		String tmp="";
-		for (int i=0; i<data.length()/2; i++) {
-			tmp=convertedData[ofs++]+""+convertedData[ofs++];
-			col=Integer.parseInt(tmp, 16);			
-			ret[dst++] = col<<16 | col<<8 | col;
-		}
-		return ret;
-	}
+    /**
+     * return a converted frame
+     * 
+     * @param frameNr
+     *            which frame to convert
+     * @param blm
+     *            to marshalled object, our source
+     * @return an image out of the frame
+     */
+    public static BlinkenImage grabFrame(int frameNr, Blm blm, int color)
+            throws NumberFormatException {
+        int frames = blm.getFrame().size();
+
+        // some sanity checks
+        if (frameNr > frames || frameNr < 0) {
+            return null;
+        }
+
+        int width = Integer.parseInt(blm.getWidth());
+        int height = Integer.parseInt(blm.getHeight());
+        int bits = Integer.parseInt(blm.getBits());
+        // int channels = Integer.parseInt(blm.getChannels());
+
+        Frame f = blm.getFrame().get(frameNr);
+        List<Row> rows = f.getRow();
+
+        BlinkenImage img = new BlinkenImage(width, height);
+
+        /**
+         * Structure of row data (http://blinkenlights.net/project/bml) Each
+         * single row describes the pixel colour values in hexadecimal notation.
+         * If the colour depth is between 1 and 4, one hexadecimal digit is used
+         * per colour value (0-f). If the colour depth is between 5 and 8, two
+         * hexadecimal digits are used per colour value (00-ff).
+         * 
+         * The is one value for each pixel, one after the other. In an RGB
+         * picture with channels="3" there are three colour values in sequence.
+         * 
+         */
+        float col = (float) (color & 255) / 255.f;
+
+        for (Row r : rows) {
+            String s = r.getvalue();
+            int[] data;
+            if (bits > 0 && bits < 5) {
+                // one char per color value
+                data = getDataFromOneCharRow(s, col);
+            } else {
+                // two char per color value
+                data = getDataFromTwoCharRow(s);
+            }
+            if (data.length != width) {
+                log.log(Level.WARNING, "Ooops: looks like here is an error: {0}!={1}",
+                        new Object[] { width, data.length });
+            }
+
+            img.addData(data);
+        }
+
+        return img;
+    }
+
+    /**
+     * convert string data to int[]
+     * 
+     * @param data
+     * @param col
+     * @return
+     */
+    private static int[] getDataFromOneCharRow(String data, float col) {
+        int[] ret = new int[data.length()];
+        int ofs = 0;
+
+        for (char c : data.toCharArray()) {
+            int i = Character.digit(c, 16);
+            ret[ofs++] = (int) (i * col * 16);
+        }
+        return ret;
+    }
+
+    /**
+     * convert string data to int[]
+     * 
+     * @param data
+     * @return
+     */
+    private static int[] getDataFromTwoCharRow(String data) {
+        int[] ret = new int[data.length() / 2];
+        char[] convertedData = data.toCharArray();
+        int ofs = 0;
+        int dst = 0;
+        int col;
+        System.out.println("TWOCHAR: " + data);
+
+        String tmp = "";
+        for (int i = 0; i < data.length() / 2; i++) {
+            tmp = convertedData[ofs++] + "" + convertedData[ofs++];
+            col = Integer.parseInt(tmp, 16);
+            ret[dst++] = col << 16 | col << 8 | col;
+        }
+        return ret;
+    }
 }
-
