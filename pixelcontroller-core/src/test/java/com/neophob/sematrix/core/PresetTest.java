9dcd81fb9a92dca45d0023d2fdb0a7a06c39e7a4
Michael Vogt <michu@neophob.com>
2014-01-07 20:28:56 +0100

    add preset test

---
+++ pixelcontroller-core/src/test/java/com/neophob/sematrix/core/PresetTest.java
@@ -0,0 +1,46 @@
+package com.neophob.sematrix.core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.neophob.sematrix.core.glue.FileUtils;
+import com.neophob.sematrix.core.glue.FileUtilsJunit;
+import com.neophob.sematrix.core.listener.MessageProcessor;
+import com.neophob.sematrix.core.output.JunitColorSet;
+import com.neophob.sematrix.core.preset.PresetService;
+import com.neophob.sematrix.core.preset.PresetServiceImpl;
+import com.neophob.sematrix.core.properties.ApplicationConfigurationHelper;
+import com.neophob.sematrix.core.properties.ValidCommand;
+import com.neophob.sematrix.core.sound.SoundDummy;
+import com.neophob.sematrix.core.visual.VisualState;
+import com.neophob.sematrix.core.visual.color.IColorSet;
+
+public class PresetTest {
+
+    private static final Logger LOG = Logger.getLogger(PresetTest.class.getName());
+
+    @Test
+    public void loadAllPresetTest() {
+        FileUtils fu = new FileUtilsJunit();
+        PresetService ps = new PresetServiceImpl(fu.getDataDir());
+        Assert.assertTrue(ps.getPresets().size() > 0);
+
+        List<IColorSet> col = new ArrayList<IColorSet>();
+        col.add(new JunitColorSet());
+        VisualState.getInstance().init(fu, new ApplicationConfigurationHelper(new Properties()),
+                new SoundDummy(), col, ps);
+
+        MessageProcessor.INSTANCE.init(ps);
+        for (int i = 0; i < ps.getPresets().size(); i++) {
+            LOG.info("Load Preset " + i);
+            ps.setSelectedPreset(i);
+            MessageProcessor.INSTANCE.processMsg(
+                    new String[] { ValidCommand.LOAD_PRESET.toString() }, false, null);
+        }
+    }
+}
